d1ca8415437ad61c6ea6fe1766d6eda886e6ca4c
dabo123148 <daniel.boden@outlook.de>
2017-04-09 06:52:22 +0200

    Bugfix

--- Spawnbarriers/Spawnbarriers.lua
+++ Spawnbarriers/Spawnbarriers.lua
@@ -1,11 +1,11 @@
 
 
-function RandomizeWastelands(game, standing)
+function Spawnbarriers(game, standing)
 	for _, territory in pairs(standing.Territories) do
         if (territory.OwnerPlayerID ~= PlayerID.Neutral) then
         local newArmiesCount = Mod.Settings.ConnectedArmyNumber;
-            if (newArmies < 0) then newArmies = 0 end;
-            if (newArmies > 100000) then newArmies = 100000 end;
+            if (newArmiesCount < 0) then newArmiesCount = 0 end;
+            if (newArmiesCount > 100000) then newArmiesCount = 100000 end;
 		for conn, _ in pairs(game.Map.Territories[territory.territoryID].ConnectedTo) do
 		   if(conn.OwnerPlayerID == PlayerID) then
 			conn.NumArmies = Armies(newArmiesCount);
