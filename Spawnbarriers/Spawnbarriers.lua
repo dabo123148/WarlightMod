048a20fc15558ee8664a7041a5ae633d9da945ed
dabo123148 <daniel.boden@outlook.de>
2017-04-09 09:00:26 +0200

    call tests

--- Spawnbarriers/Spawnbarriers.lua
+++ Spawnbarriers/Spawnbarriers.lua
@@ -6,7 +6,9 @@ function Spawnbarriers(game, standing)
         		local newArmiesCount = Mod.Settings.ConnectedArmyNumber;
            		if (newArmiesCount < 0) then newArmiesCount = 0 end;
             		if (newArmiesCount > 100000) then newArmiesCount = 100000 end;
+			print('is called1');
 			for placeholder, conn in pairs(territory.ConnectedTo) do
+				print('is called2');
 		  		if(conn.OwnerPlayerID == PlayerID.Neutral) then
 					conn.NumArmies = Armies(newArmiesCount);
 		   		end
