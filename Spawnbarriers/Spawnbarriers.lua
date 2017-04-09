beae12bd38bca2fce083186575b8dd9f2a4a6337
dabo123148 <daniel.boden@outlook.de>
2017-04-09 08:06:34 +0200

    Bugfix

--- Spawnbarriers/Spawnbarriers.lua
+++ Spawnbarriers/Spawnbarriers.lua
@@ -8,7 +8,7 @@ function Spawnbarriers(game, standing)
            		if (newArmiesCount < 0) then newArmiesCount = 0 end;
             		if (newArmiesCount > 100000) then newArmiesCount = 100000 end;
 			for conn, _ in pairs(game.Map.Territories[territoryId].ConnectedTo) do
-		  		if(conn.OwnerPlayerID == PlayerID) then
+		  		if(conn.OwnerPlayerID == PlayerID.Neutral) then
 					conn.NumArmies = Armies(newArmiesCount);
 		   		end
 	    		end
