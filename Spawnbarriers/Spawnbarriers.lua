8758ac3576660c902d9dbd349eccb6757ef05308
dabo123148 <daniel.boden@outlook.de>
2017-04-09 07:49:31 +0200

    Territoryid bug fix

--- Spawnbarriers/Spawnbarriers.lua
+++ Spawnbarriers/Spawnbarriers.lua
@@ -1,16 +1,18 @@
 
 
 function Spawnbarriers(game, standing)
+	local territoryId = 0;
 	for _, territory in pairs(standing.Territories) do
-        if (territory.OwnerPlayerID ~= PlayerID.Neutral) then
-        local newArmiesCount = Mod.Settings.ConnectedArmyNumber;
-            if (newArmiesCount < 0) then newArmiesCount = 0 end;
-            if (newArmiesCount > 100000) then newArmiesCount = 100000 end;
-		for conn, _ in pairs(game.Map.Territories[territory.territoryID].ConnectedTo) do
-		   if(conn.OwnerPlayerID == PlayerID) then
-			conn.NumArmies = Armies(newArmiesCount);
-		   end
-	     end
-        end
-    end
+        	if (territory.OwnerPlayerID ~= PlayerID.Neutral) then
+        		local newArmiesCount = Mod.Settings.ConnectedArmyNumber;
+           		if (newArmiesCount < 0) then newArmiesCount = 0 end;
+            		if (newArmiesCount > 100000) then newArmiesCount = 100000 end;
+			for conn, _ in pairs(game.Map.Territories[territoryId].ConnectedTo) do
+		  		if(conn.OwnerPlayerID == PlayerID) then
+					conn.NumArmies = Armies(newArmiesCount);
+		   		end
+	    		end
+        	end
+		territoryId += 1;
+  	end
 end
