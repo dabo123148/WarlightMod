ae2bb21ecbb09477c3b6c3ec5b681f6863d5086e
dabo123148 <daniel.boden@outlook.de>
2017-04-10 08:45:55 +0200

    IF THIS WON'T WORK, I'll surender

--- Spawnbarriers/Spawnbarriers.lua
+++ Spawnbarriers/Spawnbarriers.lua
@@ -1,18 +1,16 @@
-
-
 function Spawnbarriers(game, standing)
 	for _, territory in pairs(standing.Territories) do
-        	if (territory.OwnerPlayerID ~= PlayerID.Neutral) then
-        		local newArmiesCount = Mod.Settings.ConnectedArmyNumber;
-           		if (newArmiesCount < 0) then newArmiesCount = 0 end;
-            		if (newArmiesCount > 100000) then newArmiesCount = 100000 end;
+        if (territory.OwnerPlayerID ~= PlayerID.Neutral) then
+        	local newArmiesCount = Mod.Settings.ConnectedArmyNumber;
+           	if (newArmiesCount < 0) then newArmiesCount = 0 end;
+            if (newArmiesCount > 100000) then newArmiesCount = 100000 end;
 			print('is called1');
-			for placeholder, conn in pairs(territory.ConnectedTo) do
+			for placeholder, conn in pairs(game.Map.Territories[territory.ID].TTerritories.ConnectedTo.ID) do
 				print('is called2');
-		  		if(conn.OwnerPlayerID == PlayerID.Neutral) then
-					conn.NumArmies = Armies(newArmiesCount);
+		  		if(game.Map.Territories[territory.ID].TTerritories.OwnerPlayerID == PlayerID.Neutral) then
+					game.Map.Territories[territory.ID].TTerritories.NumArmies = Armies(newArmiesCount);
 		   		end
-	    		end
-        	end
+	    	end
+        end
   	end
 end
