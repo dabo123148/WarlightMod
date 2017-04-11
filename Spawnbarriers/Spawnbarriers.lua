0918b8e4b4c3a1e3efc7d4d0b509e9dbb738dd4d
dabo123148 <daniel.boden@outlook.de>
2017-04-11 10:40:48 +0200

    added changes

--- Spawnbarriers/Spawnbarriers.lua
+++ Spawnbarriers/Spawnbarriers.lua
@@ -10,10 +10,16 @@ function Spawnbarriers(game, standing)
 			print('is called5');
             if (newArmiesCount > 100000) then newArmiesCount = 100000 end;
 			print('is called6');
-			for placeholder, conn in pairs(game.Map.Territories[territory.ID].ConnectedTo.ID) do
-				print('is called7');
+			print(territory.ID);
+			print('is called7');
+			print(game.Map.Territories[territory.ID]);
+			print('is called8');
+			for _, ID in pairs(game.Map.Territories[territory.ID].ConnectedTo.ID) do
+				print('is called9');
 		  		if(standing.Territories[conn].OwnerPlayerID == PlayerID.Neutral) then
+					print('is called10');
 					standing.Territories[conn].NumArmies = Armies(newArmiesCount);
+					print('is called11');
 		   		end
 	    	end
         end
