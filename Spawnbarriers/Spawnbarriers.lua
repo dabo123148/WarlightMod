cc978c9d825bfc87b53f1ec775e3d6d06a5e0645
dabo123148 <daniel.boden@outlook.de>
2017-04-11 08:09:57 +0200

    Added prints

--- Spawnbarriers/Spawnbarriers.lua
+++ Spawnbarriers/Spawnbarriers.lua
@@ -1,12 +1,17 @@
 function Spawnbarriers(game, standing)
+	print('is called1');
 	for _, territory in pairs(standing.Territories) do
+		print('is called2');
         if (territory.OwnerPlayerID ~= PlayerID.Neutral) then
+			print('is called3');
         	local newArmiesCount = Mod.Settings.ConnectedArmyNumber;
+			print('is called4');
            	if (newArmiesCount < 0) then newArmiesCount = 0 end;
+			print('is called5');
             if (newArmiesCount > 100000) then newArmiesCount = 100000 end;
-			print('is called1');
+			print('is called6');
 			for placeholder, conn in pairs(game.Map.Territories[territory.ID].ConnectedTo.ID) do
-				print('is called2');
+				print('is called7');
 		  		if(standing.Territories[conn].OwnerPlayerID == PlayerID.Neutral) then
 					standing.Territories[conn].NumArmies = Armies(newArmiesCount);
 		   		end
