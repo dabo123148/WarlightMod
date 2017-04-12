0366f2333f4e4640df6ac6d48d52ada650aaa9d8
dabo123148 <daniel.boden@outlook.de>
2017-04-12 08:56:47 +0200

    added :

--- Spawnbarriers/Client_PresentSettingsUI.lua
+++ Spawnbarriers/Client_PresentSettingsUI.lua
@@ -1,6 +1,6 @@
 
 function Client_PresentSettingsUI(rootParent)
 	UI.CreateLabel(rootParent)
-		.SetText('Connected Starting Regions Armies ' .. Mod.Settings.ConnectedArmyNumber);
+		.SetText('Connected Starting Regions Armies: ' .. Mod.Settings.ConnectedArmyNumber);
 end
 
