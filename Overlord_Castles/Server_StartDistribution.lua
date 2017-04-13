6429970112c8bbc2d8cd81c089aa4510dfb4ae05
RealMelwei <melvin@weissprivat.de>
2017-04-13 10:15:25 +0200

    Add files via upload

--- Overlord_Castles/Server_StartDistribution.lua
+++ Overlord_Castles/Server_StartDistribution.lua
@@ -1,8 +1,12 @@
 function Server_StartDistribution(game, standing)
 	overriddenTerris=Mod.Settings.OverriddenTerris;
+
     for _, terr in pairs(standing.Territories) do
+		print('ID: ' .. terr.ID .. ', Arr: ' .. overriddenTerris[terr.ID]);
+
 		if not overriddenTerris[terr.ID]==nil then
 			if overriddenTerris[terr.ID]==1 and terr.IsNeutral then
+				print('Tried NumArmyChange');
 				terr.NumArmies = Mod.Settings.TroopValue;
 			end
 		end
