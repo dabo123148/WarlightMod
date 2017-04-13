40791f1f38b289306254488f116e14ba073f1ca5
dabo123148 <daniel.boden@outlook.de>
2017-04-13 10:13:53 +0200

    removed print

--- Spawnbarriers/Spawnbarriers.lua
+++ Spawnbarriers/Spawnbarriers.lua
@@ -4,7 +4,6 @@ function Spawnbarriers(game, standing)
         	local newArmiesCount = Mod.Settings.ConnectedArmyNumber;
            	if (newArmiesCount < 0) then newArmiesCount = 0 end;
             if (newArmiesCount > 100000) then newArmiesCount = 100000 end;
-			print(game.Map.Territories[territory.ID]);
 			for _, conn in pairs(game.Map.Territories[territory.ID].ConnectedTo) do
 		  		if(standing.Territories[conn.ID].OwnerPlayerID == WL.PlayerID.Neutral) then
 					standing.Territories[conn.ID].NumArmies = WL.Armies.Create(newArmiesCount);
