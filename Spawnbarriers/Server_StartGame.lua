64f7d07d3b290b083987d3ac01f64a3a7115208a
dabo123148 <daniel.boden@outlook.de>
2017-04-13 19:20:16 +0200

    bugfix

--- Spawnbarriers/Server_StartGame.lua
+++ Spawnbarriers/Server_StartGame.lua
@@ -1,10 +1,6 @@
 require('Spawnbarriers')
 
 function Server_StartGame(game, standing)
-	--Don't do anything in StartGame if we're a manual dist game (the wastelands would already have been randomized in Server_StartDistribution, we don't want to change them again or it would be a big surprise after picks)
-	if (game.Settings.AutomaticTerritoryDistribution) then
-		Spawnbarriers(game, standing);
-	end
-
+	Spawnbarriers(game, standing);
 end
 
