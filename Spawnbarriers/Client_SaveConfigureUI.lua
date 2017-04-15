e602735f878812282be5d377a562c860b37d929a
dabo123148 <daniel.boden@outlook.de>
2017-04-15 15:43:11 +0200

    Spawnbarriers V2

--- Spawnbarriers/Client_SaveConfigureUI.lua
+++ Spawnbarriers/Client_SaveConfigureUI.lua
@@ -1,4 +1,17 @@
 
 function Client_SaveConfigureUI(alert)
     Mod.Settings.ConnectedArmyNumber = InputConnectedArmyNum.GetValue();
+	Mod.Settings.Radius = InputRadius.GetValue();
+	if(Mod.Settings.ConnectedArmyNumber < 0)then
+		alert('There cannot be less than zero armies.');
+	end
+	if(Mod.Settings.ConnectedArmyNumber > 100000)then
+		alert('That are more armies than warlight supports.');
+	end
+	if(Mod.Settings.Radius < 1)then
+		alert('The Effect Radius can not be less than one, else the Mod would not have any effect and you can remove it.');
+	end
+	if(Mod.Settings.Radius > 3)then
+		alert('Through performence issuses, the maximum Effect area is limited to a radius of 3.');
+	end
 end
