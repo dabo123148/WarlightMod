9d67fec314b0822d40cd3843db9fdf70ae0223ef
dabo123148 <daniel.boden@outlook.de>
2017-04-15 15:49:44 +0200

    Spawnbarrier V2

--- Spawnbarriers/Client_PresentSettingsUI.lua
+++ Spawnbarriers/Client_PresentSettingsUI.lua
@@ -1,6 +1,6 @@
 function Client_PresentSettingsUI(rootParent)
 	UI.CreateLabel(rootParent)
-		.SetText('Connected Starting Regions Armies: ' .. Mod.Settings.ConnectedArmyNumber);
+		.SetText('Connected Starting Territory Armies: ' .. Mod.Settings.ConnectedArmyNumber);
 	UI.CreateLabel(rootParent)
 		.SetText('Effect Radius: ' .. Mod.Settings.Radius);
 end
