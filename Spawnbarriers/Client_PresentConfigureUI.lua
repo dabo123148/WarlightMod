d84e1e14c78d3d0ccf840b05f210f2cb7cb3819b
dabo123148 <daniel.boden@outlook.de>
2017-04-15 15:45:39 +0200

    Spawnbarriers V2

--- Spawnbarriers/Client_PresentConfigureUI.lua
+++ Spawnbarriers/Client_PresentConfigureUI.lua
@@ -1,4 +1,5 @@
 
+
 function Client_PresentConfigureUI(rootParent)
 	local initialValue = Mod.Settings.ConnectedArmyNumber;
 	if initialValue == nil then
@@ -6,10 +7,19 @@ function Client_PresentConfigureUI(rootParent)
 	end
     
     local horz = UI.CreateHorizontalLayoutGroup(rootParent);
-	UI.CreateLabel(horz).SetText('Connected Starting Regions Armies ');
+	UI.CreateLabel(horz).SetText('Connected Starting Territory Armies ');
     InputConnectedArmyNum = UI.CreateNumberInputField(horz)
 		.SetSliderMinValue(0)
 		.SetSliderMaxValue(100)
 		.SetValue(initialValue);
-
+	local initialValue2 = Mod.Settings.Radius;
+	if initialValue2 == nil then
+		initialValue2 = 1;
+	end
+	local horz2 = UI.CreateHorizontalLayoutGroup(rootParent);
+	UI.CreateLabel(horz2).SetText('Effect Radius ');
+    InputRadius = UI.CreateNumberInputField(horz2)
+		.SetSliderMinValue(1)
+		.SetSliderMaxValue(3)
+		.SetValue(initialValue2);
 end
