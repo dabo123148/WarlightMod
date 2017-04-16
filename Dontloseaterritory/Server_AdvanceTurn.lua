ecc7fa5fb4703213a2d2e5eee22f46b940c4591c
dabo123148 <daniel.boden@outlook.de>
2017-04-16 22:09:54 +0200

    removed ;

--- Dontloseaterritory/Server_AdvanceTurn.lua
+++ Dontloseaterritory/Server_AdvanceTurn.lua
@@ -5,7 +5,7 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
     if (order.proxyType == 'GameOrderAttackTransfer') then
 		for _, unit in pairs(order.NumArmies.SpecialUnits) do
 			skipThisOrder(WL.ModOrderControl.Skip);
-			addNewOrder(WL.GameOrderAttackTransfer.Create(order.PlayerID, order.From, order.To, order.AttackTransfer , order.ByPercent , WL.Armies.Create(order.NumArmies.NumArmies), order.AttackTeammates));
+			addNewOrder(WL.GameOrderAttackTransfer.Create(order.PlayerID, order.From, order.To, order.AttackTransfer , order.ByPercent , WL.Armies.Create(order.NumArmies.NumArmies), order.AttackTeammates))
 		end
 	end
 end
