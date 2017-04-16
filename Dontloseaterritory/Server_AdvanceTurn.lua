f38bf157c8d1543b8ec6794628aedf3c6c389c4e
dabo123148 <daniel.boden@outlook.de>
2017-04-16 21:28:10 +0200

    added prints

--- Dontloseaterritory/Server_AdvanceTurn.lua
+++ Dontloseaterritory/Server_AdvanceTurn.lua
@@ -5,17 +5,13 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
     if (order.proxyType == 'GameOrderAttackTransfer' and game.ServerGame.LatestTurnStanding.Territories[order.From].OwnerPlayerID == Mod.Settings.Spieler) then
 		for _, unit in pairs(order.NumArmies.SpecialUnits) do
 			for _, created in pairs(Mod.Settings.CreatedCommander) do
+				print('with special unit');
 				if(unit.ID == created.ID) then
+					print('with commander');
 					skipThisOrder(WL.ModOrderControl.Skip);
 					addNewOrder(WL.GameOrderAttackTransfer.Create(order.PlayerID, order.From, order.To, order.AttackTransfer , order.ByPercent , WL.Armies.Create(order.NumArmies.NumArmies), order.AttackTeammates))
 				end
 			end
 		end
 	end
-end
-
-function IsDestinationNeutral(game, order)
-	local terrID = order.To; --The order has "To" and "From" which are territory IDs
-	local terrOwner = game.ServerGame.LatestTurnStanding.Territories[terrID].OwnerPlayerID; --LatestTurnStanding always shows the current state of the game.
-	return terrOwner == WL.PlayerID.Neutral;
 end
