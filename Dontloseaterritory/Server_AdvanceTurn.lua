b8f44108f2102b208e22c4f3897e14f152010c97
dabo123148 <daniel.boden@outlook.de>
2017-04-16 21:46:32 +0200

    no special unit movement is now possible

--- Dontloseaterritory/Server_AdvanceTurn.lua
+++ Dontloseaterritory/Server_AdvanceTurn.lua
@@ -4,15 +4,8 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 	--order GameOrderAttackTransfer
     if (order.proxyType == 'GameOrderAttackTransfer' and game.ServerGame.LatestTurnStanding.Territories[order.From].OwnerPlayerID == Mod.Settings.Spieler) then
 		for _, unit in pairs(order.NumArmies.SpecialUnits) do
-			for _, created in pairs(Mod.Settings.CreatedCommander) do
-				print('with special unit');
-				print(unit.ID .. created.ID);
-				if(unit.ID == created.ID) then
-					print('with commander');
-					skipThisOrder(WL.ModOrderControl.Skip);
-					addNewOrder(WL.GameOrderAttackTransfer.Create(order.PlayerID, order.From, order.To, order.AttackTransfer , order.ByPercent , WL.Armies.Create(order.NumArmies.NumArmies), order.AttackTeammates))
-				end
-			end
+			skipThisOrder(WL.ModOrderControl.Skip);
+			addNewOrder(WL.GameOrderAttackTransfer.Create(order.PlayerID, order.From, order.To, order.AttackTransfer , order.ByPercent , WL.Armies.Create(order.NumArmies.NumArmies), order.AttackTeammates));		end
 		end
 	end
 end
