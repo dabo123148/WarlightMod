db7e2251aaeaf5fe3c930c904f3d12b07eeba006
dabo123148 <daniel.boden@outlook.de>
2017-04-16 10:48:57 +0200

    Finished Mod

--- Dontloseaterritory/NoLose.lua
+++ Dontloseaterritory/NoLose.lua
@@ -4,14 +4,14 @@ function NoLose(game, standing)
 		Territoryanzahl = Territoryanzahl+1;
 	end
 	local Zahl = math.random(0,Territoryanzahl-1);
-	while(standing.Territories[Zahl] == nil){
+	while(standing.Territories[Zahl] == nil) do
 		Zahl = math.random(0,Territoryanzahl-1);
-	}
+	end
 	while(standing.Territories[Zahl].OwnerPlayerID == WL.PlayerID.Neutral) do
 		Zahl = math.random(0,Territoryanzahl-1);
-		while(standing.Territories[Zahl] == nil){
+		while(standing.Territories[Zahl] == nil)do
 			Zahl = math.random(0,Territoryanzahl-1);
-		}
+		end
 	end
 	local territory = standing.Territories[Zahl];
 	Mod.Settings.Spieler = territory.OwnerPlayerID;
@@ -21,9 +21,18 @@ function NoLose(game, standing)
    	local newArmiesCount = Mod.Settings.StartArmies;
 	if (newArmiesCount < 0) then newArmiesCount = 0 end;
 	if (newArmiesCount > 100000) then newArmiesCount = 100000 end;
+	Mod.Settings.CreatedCommander = {};
 	for _, allterritory in pairs(standing.Territories) do
 		if(allterritory.OwnerPlayerID == Mod.Settings.Spieler) then
 			allterritory.NumArmies = WL.Armies.Create(newArmiesCount,{WL.Commander.Create(Mod.Settings.Spieler)});
+			for _, sunit in pairs(allterritory.NumArmies.SpecialUnits) do
+				Mod.Settings.CreatedCommander[tablelength(Mod.Settings.CreatedCommander)] = sunit;
+			end
 		end
 	end
+end
+function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
 end
