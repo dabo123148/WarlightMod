c1b4afe1503aed5e0b5dbbb54520f3099992b523
dabo123148 <daniel.boden@outlook.de>
2017-04-16 22:32:42 +0200

    fixed bug

--- Dontloseaterritory/Server_AdvanceTurn.lua
+++ Dontloseaterritory/Server_AdvanceTurn.lua
@@ -5,8 +5,7 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
     if (order.proxyType == 'GameOrderAttackTransfer') then
 		local Zahl = 0;
 		for _, unit in pairs(order.NumArmies.SpecialUnits) do
-			print(order.test);
-			zahl = 1;
+			Zahl = 1;
 		end
 		if(Zahl == 1) then
 			skipThisOrder(WL.ModOrderControl.Skip);
