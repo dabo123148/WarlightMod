98d60e89aee903ff46c1148ebaf5075707e06a48
dabo123148 <daniel.boden@outlook.de>
2017-04-16 11:01:52 +0200

    removed prints

--- Dontloseaterritory/Server_AdvanceTurn.lua
+++ Dontloseaterritory/Server_AdvanceTurn.lua
@@ -4,11 +4,8 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 	--order GameOrderAttackTransfer
     if (order.proxyType == 'GameOrderAttackTransfer' and game.ServerGame.LatestTurnStanding.Territories[order.From].OwnerPlayerID == Mod.Settings.Spieler) then
 		for _, unit in pairs(order.NumArmies.SpecialUnits) do
-			print(unit.ID);
 			for _, created in pairs(Mod.Settings.CreatedCommander) do
-			print('T1');
 				if(unit.ID == created.ID) then
-					print('T2');
 					skipThisOrder(WL.ModOrderControl.Skip);
 					addNewOrder(WL.GameOrderAttackTransfer.Create(order.PlayerID, order.From, order.To, order.AttackTransfer , order.ByPercent , WL.Armies.Create(order.NumArmies.NumArmies), order.AttackTeammates))
 				end
