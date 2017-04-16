748027048e531981e1afac78287ed962722d87b0
dabo123148 <daniel.boden@outlook.de>
2017-04-16 22:13:14 +0200

    change

--- Dontloseaterritory/Server_AdvanceTurn.lua
+++ Dontloseaterritory/Server_AdvanceTurn.lua
@@ -3,7 +3,11 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 	--result GameOrderAttackTransferResult
 	--order GameOrderAttackTransfer
     if (order.proxyType == 'GameOrderAttackTransfer') then
+		local Zahl = 0;
 		for _, unit in pairs(order.NumArmies.SpecialUnits) do
+			zahl = 1;
+		end
+		if(Zahl == 1) then
 			skipThisOrder(WL.ModOrderControl.Skip);
 			addNewOrder(WL.GameOrderAttackTransfer.Create(order.PlayerID, order.From, order.To, order.AttackTransfer , order.ByPercent , WL.Armies.Create(order.NumArmies.NumArmies), order.AttackTeammates))
 		end
