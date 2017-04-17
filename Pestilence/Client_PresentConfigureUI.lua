c38b2e08db9c1106d7d44fee53c9b1479d34dcfb
dabo123148 <daniel.boden@outlook.de>
2017-04-17 19:26:43 +0200

    changed init and min value

--- Pestilence/Client_PresentConfigureUI.lua
+++ Pestilence/Client_PresentConfigureUI.lua
@@ -1,13 +1,13 @@
 
 function Client_PresentConfigureUI(rootParent)
 	local initialValue1 = Mod.Settings.PestilenceStrength;
-	if initialValue1 == nil then initialValue1 = 1; end
+	if initialValue1 == nil then initialValue1 = 5; end
     
 
     local horz1 = UI.CreateHorizontalLayoutGroup(rootParent);
 	UI.CreateLabel(horz1).SetText('Pestilence Strength');
     numberInputField1 = UI.CreateNumberInputField(horz1)
-		.SetSliderMinValue(0)
+		.SetSliderMinValue(1)
 		.SetSliderMaxValue(200)
 		.SetValue(initialValue1);
 
