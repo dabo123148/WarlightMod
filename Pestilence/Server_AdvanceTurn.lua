ac859b2be6f2e6294a43bbd2753bce764878b771
dabo123148 <daniel.boden@outlook.de>
2017-04-17 08:43:15 +0200

    standing.Territories changed to pairs(standing.Territories)

--- Pestilence/Server_AdvanceTurn.lua
+++ Pestilence/Server_AdvanceTurn.lua
@@ -1,7 +1,7 @@
 
 function Server_AdvanceTurn_End(game,addOrder)
     standing=game.ServerGame.LatestTurnStanding;
-	for _,terr in standing.Territories do
+	for _,terr in pairs(standing.Territories) do
 		if not (terr.isNeutral) then
 			terr.NumArmies.Substract(math.min(Mod.Settings.PestilenceStrength,terr.numArmies.numArmies));
 			if (terr.NumArmies.NumArmies==0) then
