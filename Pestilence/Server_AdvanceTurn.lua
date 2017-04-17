0d925bf5ca92eeffbf31a958ead103f18a1e6be5
dabo123148 <daniel.boden@outlook.de>
2017-04-17 08:52:38 +0200

    changed line 6

--- Pestilence/Server_AdvanceTurn.lua
+++ Pestilence/Server_AdvanceTurn.lua
@@ -3,7 +3,7 @@ function Server_AdvanceTurn_End(game,addOrder)
     standing=game.ServerGame.LatestTurnStanding;
 	for _,terr in pairs(standing.Territories) do
 		if not (terr.IsNeutral) then
-			terr.NumArmies = WL.Armies.Create(terr.NumArmies.NumArmies-Mod.Settings.PestilenceStrength);
+			game.ServerGame.LatestTurnStanding.Territories[terr.ID].NumArmies = WL.Armies.Create(terr.NumArmies.NumArmies-Mod.Settings.PestilenceStrength);
 			if (terr.NumArmies.NumArmies<=0) then
 				terr.OwnerPlayerID = WL.PlayerID.Neutral;
 			end
