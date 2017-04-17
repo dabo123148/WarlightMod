093c22a0f5438c00cf9f35e27088607098835742
RealMelwei <melvin@weissprivat.de>
2017-04-17 20:22:10 +0200

    GameOrder Array
    Now all Pestilence Orders are put into 1.

--- Pestilence/Server_AdvanceTurn.lua
+++ Pestilence/Server_AdvanceTurn.lua
@@ -1,19 +1,26 @@
 
 function Server_AdvanceTurn_End(game,addOrder)
     standing=game.ServerGame.LatestTurnStanding;
+	CurrentIndex=0;
+	PestilenceOrder={};
 	for _,terr in pairs(standing.Territories) do
 		if not (terr.IsNeutral) then
 			Count = terr.NumArmies.NumArmies;
 			terrMod2=WL.TerritoryModification.Create(terr.ID);
 			terrMod2.SetArmiesTo=math.max(Count-Mod.Settings.PestilenceStrength,0);
-			addOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID,'Pestilence',{},{terrMod2}));
+			PestilenceOrder[CurrentIndex]=terrMod2;
+			CurrentIndex=CurrentIndex+1;
+			--addOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID,'Pestilence',{},{terrMod2}));
 			if (Count<=Mod.Settings.PestilenceStrength) then
 				terrMod = WL.TerritoryModification.Create(terr.ID);
 				terrMod.SetOwnerOpt=WL.PlayerID.Neutral;
-				addOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID,"Pestilence",{},{terrMod}));
+				PestilenceOrder[CurrentIndex]=terrMod;
+				CurrentIndex=CurrentIndex+1;
+				--addOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID,"Pestilence",{},{terrMod}));
 			end
 			
 		end
 	end
+	addOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID,'Pestilence',{},PestilenceOrder));
 
 end
