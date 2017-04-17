f77b5822e47fc86e835873a856030b39da4da66f
dabo123148 <daniel.boden@outlook.de>
2017-04-17 14:31:14 +0200

    try

--- LimitedMultiattack/Server_AdvanceTurn.lua
+++ LimitedMultiattack/Server_AdvanceTurn.lua
@@ -1,18 +1,26 @@
-
+function Server_AdvanceTurn_Start (game,addNewOrder)
+	UbrigeAngriffe={};
+	local Maximaleangriffe = Mod.Settings.MaxAttacks;
+	if (Maximaleangriffe < 1) then Maximaleangriffe = 1 end;
+	if (Maximaleangriffe > 100000) then Maximaleangriffe = 100000 end;
+	for _, terr in pairs(game.Map.Territories) do
+		UbrigeAngriffe[terr.ID] = Maximaleangriffe;
+	end
+end
 function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
 	--result GameOrderAttackTransferResult
 	--order GameOrderAttackTransfer
 	if(order.proxyType == 'GameOrderAttackTransfer') then
-		if(Mod.Settings.UbrigeAngriffe[order.From] > 0)then
+		if(UbrigeAngriffe[order.From] > 0)then
 			if(result.IsSuccessful)then
 				if(game.ServerGame.LatestTurnStanding.Territories[order.From].OwnerPlayerID ~= game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID)then
-					Mod.Settings.UbrigeAngriffe[order.To] = Mod.Settings.UbrigeAngriffe[order.From];
+					UbrigeAngriffe[order.To] = UbrigeAngriffe[order.From];
 				else
-					Mod.Settings.UbrigeAngriffe[order.To] = 1;
+					UbrigeAngriffe[order.To] = 1;
 				end
-				Mod.Settings.UbrigeAngriffe[order.To] = Mod.Settings.UbrigeAngriffe[order.To] - 1;
+				UbrigeAngriffe[order.To] = UbrigeAngriffe[order.To] - 1;
 			else
-				Mod.Settings.UbrigeAngriffe[order.From] = 0;
+				UbrigeAngriffe[order.From] = 0;
 			end
 		else
 			skipThisOrder(WL.ModOrderControl.Skip);
@@ -24,6 +32,6 @@ function Server_AdvanceTurn_End(game,addNewOrder)
 	if (Maximaleangriffe < 1) then Maximaleangriffe = 1 end;
 	if (Maximaleangriffe > 100000) then Maximaleangriffe = 100000 end;
 	for _, terr in pairs(game.Map.Territories) do
-		Mod.Settings.UbrigeAngriffe[terr.ID] = Maximaleangriffe;
+		UbrigeAngriffe[terr.ID] = Maximaleangriffe;
 	end
 end
