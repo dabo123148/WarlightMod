f2a4c84fe1bfcfe709914d184820149766d4811a
dabo123148 <daniel.boden@outlook.de>
2017-04-17 08:45:24 +0200

    isNeutral changed to IsNeutral

--- Pestilence/Server_AdvanceTurn.lua
+++ Pestilence/Server_AdvanceTurn.lua
@@ -2,7 +2,7 @@
 function Server_AdvanceTurn_End(game,addOrder)
     standing=game.ServerGame.LatestTurnStanding;
 	for _,terr in pairs(standing.Territories) do
-		if not (terr.isNeutral) then
+		if not (terr.IsNeutral) then
 			terr.NumArmies.Substract(math.min(Mod.Settings.PestilenceStrength,terr.numArmies.numArmies));
 			if (terr.NumArmies.NumArmies==0) then
 				terr.OwnerPlayerID = WL.PlayerID.Neutral;
