8c01538ec7918c73d4c3d7d81913845d267d7042
RealMelwei <melvin@weissprivat.de>
2017-04-17 18:04:07 +0200

    Jetzt aber.........

--- Pestilence/Server_AdvanceTurn.lua
+++ Pestilence/Server_AdvanceTurn.lua
@@ -3,7 +3,6 @@ function Server_AdvanceTurn_End(game,addOrder)
     standing=game.ServerGame.LatestTurnStanding;
 	for _,terr in pairs(standing.Territories) do
 		if not (terr.IsNeutral) then
-			terr.NumArmies.Substract(math.min(Mod.Settings.PestilenceStrength,terr.numArmies.numArmies));
 			if (terr.NumArmies.NumArmies==0) then
 				addOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID,'Pestilence',nil,WL.TerritoryModification.Create(terr.ID).SetArmiesTo(math.min(terr.NumArmies.NumArmies-Mod.Settings.PestilenceStrength,0))));
 				if (terr.NumArmies.NumArmies<=Mod.Settings.PestilenceStrength) then
