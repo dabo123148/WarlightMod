461575ef0c81e6ca876f734fcc0b204965f912d6
RealMelwei <melvin@weissprivat.de>
2017-04-17 18:18:52 +0200

    Add files via upload

--- Pestilence/Server_AdvanceTurn.lua
+++ Pestilence/Server_AdvanceTurn.lua
@@ -5,7 +5,9 @@ function Server_AdvanceTurn_End(game,addOrder)
 		if not (terr.IsNeutral) then
 			--addOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID,'Pestilence',nil,WL.TerritoryModification.Create(terr.ID).SetArmiesTo(math.min(terr.NumArmies.NumArmies-Mod.Settings.PestilenceStrength,0))));
 			if (terr.NumArmies.NumArmies<=Mod.Settings.PestilenceStrength) then
-				addOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID,'Pestilence',nil,WL.TerritoryModification.Create(terr.ID).SetOwnerOpt(WL.PlayerID.Neutral)));
+				terrMod = WL.TerritoryModification.Create(terr.ID);
+				terrMod.SetOwnerOpt=WL.PlayerID.Neutral;
+				addOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID,'Pestilence',nil,terrMod));
 			end
 			
 		end
