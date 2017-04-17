0f3329891e527325ca515892e11c0a70328b5f31
dabo123148 <daniel.boden@outlook.de>
2017-04-17 07:59:56 +0200

    added comments

--- Dontloseaterritory/NoLose.lua
+++ Dontloseaterritory/NoLose.lua
@@ -1,8 +1,7 @@
 function NoLose(game, standing)
-	local Territoryanzahl = 0;
-	for _, terr in pairs(standing.Territories) do
-		Territoryanzahl = Territoryanzahl+1;
-	end
+	--Couting territorys
+	local Territoryanzahl = tablelength(standing.Territories);
+	--picking a random not neutral territory
 	local Zahl = math.random(0,Territoryanzahl-1);
 	while(standing.Territories[Zahl] == nil) do
 		Zahl = math.random(0,Territoryanzahl-1);
@@ -13,8 +12,9 @@ function NoLose(game, standing)
 			Zahl = math.random(0,Territoryanzahl-1);
 		end
 	end
+	--giving every starting territory of the owner of the random player a commander
 	local territory = standing.Territories[Zahl];
-	Mod.Settings.Spieler = territory.OwnerPlayerID;
+	Mod.Settings.Spieler = territory.OwnerPlayerID;--Saving the player(must be changed)
 	if(Mod.Settings.StartArmies == nil) then
 		Mod.Settings.StartArmies=5;
 	end
@@ -24,9 +24,12 @@ function NoLose(game, standing)
 	for _, allterritory in pairs(standing.Territories) do
 		if(allterritory.OwnerPlayerID == Mod.Settings.Spieler) then
 			allterritory.NumArmies = WL.Armies.Create(newArmiesCount,{WL.Commander.Create(Mod.Settings.Spieler)});
+			--In a earlyer version of the mod, I saved the commander id for checking if the commander who tries to move is
+			--one of the commander I created
 		end
 	end
 end
+--Counting the elements in a table
 function tablelength(T)
   local count = 0
   for _ in pairs(T) do count = count + 1 end
