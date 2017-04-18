7406d16cb2d8d42134665027565a3cfde7832fd9
dabo123148 <daniel.boden@outlook.de>
2017-04-18 17:31:53 +0200

    changed array start

--- NeutralMoves/Server_AdvanceTurn.lua
+++ NeutralMoves/Server_AdvanceTurn.lua
@@ -13,10 +13,10 @@ function Server_AdvanceTurn_End(game,addNewOrder)
 						if(Takenarmies > 0)then
 							Remainingarmies = Remainingarmies - Takenarmies;
 							local terrMod = {}
-							terrMod[0] = WL.TerritoryModification.Create(terr.ID);
-							terrMod[0].SetArmiesTo = Remainingarmies;
-							terrMod[1] = WL.TerritoryModification.Create(conn.ID);
-							terrMod[1].SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[conn.ID].NumArmies.NumArmies + Takenarmies;
+							terrMod[1] = WL.TerritoryModification.Create(terr.ID);
+							terrMod[1].SetArmiesTo = Remainingarmies;
+							terrMod[2] = WL.TerritoryModification.Create(conn.ID);
+							terrMod[2].SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[conn.ID].NumArmies.NumArmies + Takenarmies;
 							addNewOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral, 'Neutral_Move', nil, terrMod));
 						end
 					end
