a5250c4e2ce6ae0eff7b39fc33ef845696483719
dabo123148 <daniel.boden@outlook.de>
2017-04-19 21:23:37 +0200

    replaced array through string seperated with ,

--- OneWayConnections/Client_SaveConfigureUI.lua
+++ OneWayConnections/Client_SaveConfigureUI.lua
@@ -2,21 +2,38 @@
 function Client_SaveConfigureUI(alert)
 	print('Test');
     Mod.Settings.TotalRemovedConnections = InputStartArmies.GetValue();
-	Mod.Settings.RemovedConnections = {};
+	Mod.Settings.RemovedConnections = "";
 	local num = 1;
 	local setnum = 1;
 	print('Test2');
-	--while(num < InputStartArmies.GetValue()*2) do
-	--	print(RemovedConnectionsFields[num]);
-	--	print(RemovedConnectionsFields[num].GetText());
-	--	if(RemovedConnectionsFields[num].GetText() ~= nil and RemovedConnectionsFields[num+1].GetText() ~= nil)then
-	--		print('Test3');
-	--		print(RemovedConnectionsFields[num].GetText());
-	---		Mod.Settings.RemovedConnections[setnum] = RemovedConnectionsFields[num].GetText();
-	--		Mod.Settings.RemovedConnections[setnum+1] = RemovedConnectionsFields[num+1].GetText();
-	--		setnum = setnum +2;
-	--	end
-	--	num = num+2;
-	--end
+	while(num < InputStartArmies.GetValue()*2) do
+		print(RemovedConnectionsFields[num]);
+		print(RemovedConnectionsFields[num].GetText());
+		if(RemovedConnectionsFields[num].GetText() ~= nil and RemovedConnectionsFields[num+1].GetText() ~= nil)then
+			print('Test3');
+			bool Vorhanden = false;
+			for _, elem in pairs(RemovedConnectionsFields[num].GetText())do
+				if(elem == ',')then
+					print('Test6');
+					Vorhanden=true;
+				end
+			end
+			for _, elem in pairs(RemovedConnectionsFields[num+1].GetText())do
+				if(elem == ',')then
+					print('Test5');
+					Vorhanden=true;
+				end
+			end
+			print(Vorhanden);
+			if(Vorhanden == false)then
+				print(RemovedConnectionsFields[num].GetText());
+				Mod.Settings.RemovedConnections = Mod.Settings.RemovedConnections .. ',' ..RemovedConnectionsFields[num].GetText() .. ',';
+				Mod.Settings.RemovedConnections = Mod.Settings.RemovedConnections .. RemovedConnectionsFields[num+1].GetText();
+				setnum = setnum +2;
+			end
+		end
+		num = num+2;
+	end
+	print(Mod.Settings.RemovedConnections);
 	print('Test4');
 end
