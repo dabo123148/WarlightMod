053ae7e7a67d33c57363ae9a8adbf9b215bf5fc5
dabo123148 <daniel.boden@outlook.de>
2017-04-19 22:01:12 +0200

    bugfix

--- OneWayConnections/Client_SaveConfigureUI.lua
+++ OneWayConnections/Client_SaveConfigureUI.lua
@@ -7,44 +7,44 @@ function Client_SaveConfigureUI(alert)
 	local setnum = 1;
 	print('Test2');
 	while(num < InputStartArmies.GetValue()*2) do
-		print(RemovedConnectionsFields[num]);
-		print(RemovedConnectionsFields[num].GetText());
-		if(RemovedConnectionsFields[num].GetText() ~= nil and RemovedConnectionsFields[num+1].GetText() ~= nil)then
-			print('Test3');
-			local Vorhanden = false;
-			local chartable = {};
-			local ausgang = RemovedConnectionsFields[num].GetText();
-			while(string.len(ausgang)>0)do
-				chartable[tablelength(chartable)] = string.sub(ausgang, 1 , 1);
-				ausgang = string.sub(ausgang, 2);
-			end
-			for _, elem in pairs(chartable)do
-				if(elem == ',')then
-					print('Test6');
-					Vorhanden=true;
+		if(RemovedConnectionsFields[num] ~= nil)then
+			if(RemovedConnectionsFields[num].GetText() ~= nil and RemovedConnectionsFields[num+1].GetText() ~= nil)then
+				print('Test3');
+				local Vorhanden = false;
+				local chartable = {};
+				local ausgang = RemovedConnectionsFields[num].GetText();
+				while(string.len(ausgang)>0)do
+					chartable[tablelength(chartable)] = string.sub(ausgang, 1 , 1);
+					ausgang = string.sub(ausgang, 2);
+				end
+				for _, elem in pairs(chartable)do
+					if(elem == ',')then
+						print('Test6');
+						Vorhanden=true;
+					end
+				end
+				chartable = {};
+				ausgang = RemovedConnectionsFields[num+1].GetText();
+				while(string.len(ausgang)>0)do
+					chartable[tablelength(chartable)] = string.sub(ausgang, 1 , 1);
+					ausgang = string.sub(ausgang, 2);
+				end
+				for _, elem in pairs(chartable)do
+					if(elem == ',')then
+						print('Test5');
+						Vorhanden=true;
+					end
+				end
+				print(Vorhanden);
+				if(Vorhanden == false)then
+					print(RemovedConnectionsFields[num].GetText());
+					Mod.Settings.RemovedConnections = Mod.Settings.RemovedConnections .. ',' ..RemovedConnectionsFields[num].GetText() .. ',';
+					Mod.Settings.RemovedConnections = Mod.Settings.RemovedConnections .. RemovedConnectionsFields[num+1].GetText();
+					setnum = setnum +2;
 				end
 			end
-			chartable = {};
-			ausgang = RemovedConnectionsFields[num+1].GetText();
-			while(string.len(ausgang)>0)do
-				chartable[tablelength(chartable)] = string.sub(ausgang, 1 , 1);
-				ausgang = string.sub(ausgang, 2);
-			end
-			for _, elem in pairs(chartable)do
-				if(elem == ',')then
-					print('Test5');
-					Vorhanden=true;
-				end
-			end
-			print(Vorhanden);
-			if(Vorhanden == false)then
-				print(RemovedConnectionsFields[num].GetText());
-				Mod.Settings.RemovedConnections = Mod.Settings.RemovedConnections .. ',' ..RemovedConnectionsFields[num].GetText() .. ',';
-				Mod.Settings.RemovedConnections = Mod.Settings.RemovedConnections .. RemovedConnectionsFields[num+1].GetText();
-				setnum = setnum +2;
-			end
+			num = num+2;
 		end
-		num = num+2;
 	end
 	print(Mod.Settings.RemovedConnections);
 	print('Test4');
