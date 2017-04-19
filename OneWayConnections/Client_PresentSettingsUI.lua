4f51014a9ce9e16dca8f78a74089956dec1c2f52
dabo123148 <daniel.boden@outlook.de>
2017-04-19 22:12:10 +0200

    added settings

--- OneWayConnections/Client_PresentSettingsUI.lua
+++ OneWayConnections/Client_PresentSettingsUI.lua
@@ -1,6 +1,33 @@
 
 function Client_PresentSettingsUI(rootParent)
-	UI.CreateLabel(rootParent)
-		.SetText('Commanderplayer StartArmies: ' .. Mod.Settings.StartArmies);
+	local RC = stringtotable(Mod.Settings.RemovedConnections);
+	local num = 0;
+	while(RC[num] ~= nil)do
+		UI.CreateLabel(rootParent)
+		.SetText('Removed Connection between ' .. RC[num] .. ' and ' RC[num+1]);
+		num=num+2;
+	end
 end
-
+function stringtotable(variable)
+	local chartable = {};
+	while(string.len(variable)>0)do
+		chartable[tablelength(chartable)] = string.sub(variable, 1 , 1);
+		variable = string.sub(variable, 2);
+	end
+	local newtable = {};
+	local tablepos = 0;
+	for _, elem in pairs(chartable)do
+		if(elem == ',')then
+			tablepos = tablepos + 1;
+			newtable[tablepos] = "";
+		else
+			newtable[tablepos] = newtable[tablepos] .. elem;
+		end
+	end
+	return newtable;
+end
+function tablelength(T)
+	local count = 0;
+	for _ in pairs(T) do count = count + 1 end;
+	return count;
+end
