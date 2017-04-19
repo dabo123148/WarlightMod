94eee31a933ea41d1d2f2c3ed2c2afbb69311878
dabo123148 <daniel.boden@outlook.de>
2017-04-19 22:10:19 +0200

    bugfix

--- OneWayConnections/Server_AdvanceTurn.lua
+++ OneWayConnections/Server_AdvanceTurn.lua
@@ -9,9 +9,11 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 		local Fromterrname = string.upper(game.Map.Territories[order.From].Name);
 		local Toterrname = string.upper(game.Map.Territories[order.To].Name);
 		while(num < Mod.Settings.TotalRemovedConnections*2 and Match == false)do
-			if(string.upper(removedconns[num]) == Fromterrname and string.upper(removedconns[num+1]) == Toterrname)then
-				Match = true;
-				skipThisOrder(WL.ModOrderControl.Skip);
+			if(removedconns[num] ~=nil)then
+				if(string.upper(removedconns[num]) == Fromterrname and string.upper(removedconns[num+1]) == Toterrname)then
+					Match = true;
+					skipThisOrder(WL.ModOrderControl.Skip);
+				end
 			end
 			num = num + 2;
 		end
