6b22056ebde0f47411f3d174a5843de9fa0a1130
dabo123148 <daniel.boden@outlook.de>
2017-04-21 12:00:35 +0200

    bugfix

--- DeployLimit/Server_AdvanceTurn.lua
+++ DeployLimit/Server_AdvanceTurn.lua
@@ -8,17 +8,14 @@ function Server_AdvanceTurn_Start (game,addNewOrder)
 	SkippedOrders = {};
 end
 function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
-	print('Test1');
 	if(executed == false)then
 		if(order.proxyType == 'GameOrderDeploy') then
 			local Deploys = order.NumArmies;
 			local on = order.DeployOn;
-			print('Test2');
 			if(AlreadyDeployed[on] + Deploys > Mod.Settings.MaxDeploy)then
-				print('Test3');
 				Deploys = Mod.Settings.MaxDeploy-AlreadyDeployed[on];
 				if(Deploys > 0)then
-					addNewOrder(order.PlayerID,Deploys,on);
+					addNewOrder(WL.GameOrderDeploy.Create(order.PlayerID,Deploys,on));
 					redeployarmies[order.PlayerID] = redeployarmies[order.PlayerID] + (order.NumArmies-Deploys);--This armies must be deployed on other Territories
 					skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage);
 				end
@@ -26,14 +23,12 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 				AlreadyDeployed[on] = Deploys;
 			end
 		else
-			print('Test4');
 			SkippedOrders[tablelength(SkippedOrders)] = order;
 			skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage);
 		end
 	end
 end
 function Server_AdvanceTurn_End (game,addNewOrder)
-	print('Test5');
 	if(executed == false)then
 		executed = true;
 		for _, terri in pairs(game.ServerGame.LatestTurnStanding)do
