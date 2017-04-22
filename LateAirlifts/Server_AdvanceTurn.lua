e215ff9e52bf3abb09ff86a66a4a8db5b073beb8
dabo123148 <daniel.boden@outlook.de>
2017-04-22 10:31:24 +0200

    bugfix

--- LateAirlifts/Server_AdvanceTurn.lua
+++ LateAirlifts/Server_AdvanceTurn.lua
@@ -14,8 +14,8 @@ function Server_AdvanceTurn_End(game,addNewOrder)
 	if(executed == false) then
 		executed = true;
 		for _,order in pairs(SkippedAirlifts)do
-			if(order.PlayerID == game.ServerGame.LatestTurnStanding[order.From].OwnerPlayerID)then
-				if(game.ServerGame.LatestTurnStanding[order.From].OwnerPlayerID == game.ServerGame.LatestTurnStanding[order.To].OwnerPlayerID)then
+			if(order.PlayerID == game.ServerGame.LatestTurnStanding.Territories[order.FromTerritoryID].OwnerPlayerID)then
+				if(game.ServerGame.LatestTurnStanding.Territories[order.FromTerritoryID].OwnerPlayerID == game.ServerGame.LatestTurnStanding.Territories[order.ToTerritoryID].OwnerPlayerID)then
 					addNewOrder(order);
 				end
 			end
