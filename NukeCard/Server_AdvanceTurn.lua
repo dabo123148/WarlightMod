34673a4ddb5ecc6abf63f17b879c0217a4db850f
dabo123148 <daniel.boden@outlook.de>
2017-04-22 17:22:40 +0200

    bugfix

--- NukeCard/Server_AdvanceTurn.lua
+++ NukeCard/Server_AdvanceTurn.lua
@@ -4,31 +4,16 @@ function Server_AdvanceTurn_Start (game,addNewOrder)
 end
 function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
 	if(executed == false)then
-		if(order.proxyType ~= 'GameOrderDeploy')then--deployorders needn't to be skipped
-			SkippedOrders[tablelength(SkippedOrders)] = order;
+		if(order.proxyType ~= 'GameOrderPlayCardReconnaissance')
 			skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage);
-			print(order.proxyType);
-		end
-	end
-end
-function Server_AdvanceTurn_End(game, addNewOrder)
-	if(executed == false)then
-		print('Test2');
-		executed = true;
-		for _,order in pairs(SkippedOrders) do
-			if(order.proxyType == 'GameOrderPlayCardReconnaissance')then
-				local Effect = {};
-				for _,conn in pairs(game.Map.Territories[order.TargetTerritory].ConnectedTo) do
-					Effect[tablelength(Effect)+1] = WL.TerritoryModification.Create(conn.ID);
-					Effect[tablelength(Effect)].SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[conn.ID].NumArmies.NumArmies*0.75;
-				end
-				Effect[tablelength(Effect)+1] = WL.TerritoryModification.Create(order.TargetTerritory);
-				Effect[tablelength(Effect)].SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[order.TargetTerritory].NumArmies.NumArmies/2;
-				addNewOrder(WL.GameOrderEvent.Create(order.PlayerID,'Nuked ' .. game.Map.Territories[order.TargetTerritory].Name,{},Effect));
+			local Effect = {};
+			for _,conn in pairs(game.Map.Territories[order.TargetTerritory].ConnectedTo) do
+				Effect[tablelength(Effect)+1] = WL.TerritoryModification.Create(conn.ID);
+				Effect[tablelength(Effect)].SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[conn.ID].NumArmies.NumArmies*0.75;
 			end
-		end
-		for _,order in pairs(SkippedOrders) do
-			addNewOrder(order);
+			Effect[tablelength(Effect)+1] = WL.TerritoryModification.Create(order.TargetTerritory);
+			Effect[tablelength(Effect)].SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[order.TargetTerritory].NumArmies.NumArmies/2;
+			addNewOrder(WL.GameOrderEvent.Create(order.PlayerID,'Nuked ' .. game.Map.Territories[order.TargetTerritory].Name,{},Effect));
 		end
 	end
 end
