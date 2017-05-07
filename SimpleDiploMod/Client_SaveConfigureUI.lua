e8a334828456027e43afc9f26cf8b8b4b251ad1d
dabo123148 <dabo123148@users.noreply.github.com>
2017-05-07 13:10:53 +0200

    update

--- SimpleDiploMod/Client_SaveConfigureUI.lua
+++ SimpleDiploMod/Client_SaveConfigureUI.lua
@@ -1,7 +1,15 @@
 
 function Client_SaveConfigureUI(alert)
-    Mod.Settings.AllowAIDeclaration = AIDeclerationcheckbox.GetIsChecked();
+  	Mod.Settings.AllowAIDeclaration = AIDeclerationcheckbox.GetIsChecked();
 	if(Mod.Settings.AllowAIDeclaration == nil)then
 		Mod.Settings.AllowAIDeclaration = true;
+	end
+	Mod.Settings.SeeAllyTerritories = SeeAllyTerritoriesCheckbox.GetIsChecked();
+	if(Mod.Settings.SeeAllyTerritories == nil)then
+		Mod.Settings.SeeAllyTerritories = true;
+	end
+	Mod.Settings.PublicAllies = PublicAlliesCheckbox.GetIsChecked();
+	if(Mod.Settings.PublicAllies == nil)then
+		Mod.Settings.PublicAllies = true;
 	end
 end
