7734cf64f75b60c5ce66b7b1a9338bf735a71429
dabo123148 <dabo123148@users.noreply.github.com>
2017-05-10 15:40:14 +0200

    update to V2.0

--- Dontloseaterritory/Server_AdvanceTurn.lua
+++ Dontloseaterritory/Server_AdvanceTurn.lua
@@ -1,18 +1,59 @@
 
 function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
-	--result GameOrderAttackTransferResult
-	--order GameOrderAttackTransfer
     if (order.proxyType == 'GameOrderAttackTransfer') then
-		--Checking if a territory has a commmander
-		local Zahl = 0;
-		for _, unit in pairs(order.NumArmies.SpecialUnits) do
-			--in an earlyer version I checked if unit.ID == Mod.Settings and then removed the order and adapted it
-			Zahl = 1;
-		end
-		if(Zahl == 1) then
-			--Skiping the order, if a special unit is part of an attack
-			skipThisOrder(WL.ModOrderControl.Skip);
-			addNewOrder(WL.GameOrderAttackTransfer.Create(order.PlayerID, order.From, order.To, order.AttackTransfer , order.ByPercent , WL.Armies.Create(order.NumArmies.NumArmies), order.AttackTeammates));
+		if(result.IsSuccessful and result.IsAttack)then
+			--Should still support a little bit the older versions
+			if(Mod.Settings.NoTerritory == nil)then
+				for _,terr in pairs(game.ServerGame.TurnZeroStanding.Territories)do
+					if(terr.ID == order.To)then
+						if(terr.OwnerPlayerID ~= WL.PlayerID.Neutral)then
+							skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage);
+							addNewOrder(Eliminate(game,game.ServerGame.LatestTurnStanding.Territories[order.To]));
+							addNewOrder(order);
+						end
+					end
+				end
+			end
+			if(Mod.Settings.NoTerritory == false)then
+				for _,terr in pairs(game.ServerGame.TurnZeroStanding.Territories)do
+					if(terr.ID == order.To)then
+						if(terr.OwnerPlayerID ~= WL.PlayerID.Neutral)then
+							skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage);
+							addNewOrder(Eliminate(game,game.ServerGame.LatestTurnStanding.Territories[order.To]));
+							addNewOrder(order);
+						end
+					end
+				end
+			else
+				skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage);
+				addNewOrder(Eliminate(game,game.ServerGame.LatestTurnStanding.Territories[order.To]));
+				addNewOrder(order);
+			end
 		end
 	end
+end
+function Eliminate(game,Player)
+	local Effect = {};
+	for _,terr in pairs(game.ServerGame.LatestTurnStanding.Territories)do
+		if(Player==terr.OwnerPlayerID)then
+			Effect[tablelength(t)+1] = WL.TerritoryModification.Create(terr.ID); 
+			Effect[tablelength(t)].SetOwnerOpt = WL.PlayerID.Neutral;
+		end
+	end
+	return WL.GameOrderEvent.Create(Player, "Got eliminated through losing a territory", nil, Effect);
+end
+function Exists(game,Player)
+	for _,terr in pairs(game.ServerGame.LatestTurnStanding.Territories)do
+		if(Player==terr.OwnerPlayerID)then
+			return true;
+		end
+	end
+	return false;
+end
+function tablelength(t)
+	local count = 0;
+	for _,elem in pairs(t)do
+		count = count + 1;
+	end
+	return count;
 end
