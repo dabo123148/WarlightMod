c893310251884348fbc8e06343ac466bcc298e5b
dabo123148 <dabo123148@users.noreply.github.com>
2017-05-10 15:24:51 +0200

    update

--- Dontloseaterritory/Client_PresentSettingsUI.lua
+++ Dontloseaterritory/Client_PresentSettingsUI.lua
@@ -1,6 +1,16 @@
 
 function Client_PresentSettingsUI(rootParent)
-	UI.CreateLabel(rootParent)
-		.SetText('Commanderplayer StartArmies: ' .. Mod.Settings.StartArmies);
+	if(Mod.Settings.NoTerritory ~= nil)then
+		if(Mod.Settings.NoTerritory)then
+			UI.CreateLabel(rootParent)
+			.SetText('If a player loses a territory, he has lost');
+		else
+			UI.CreateLabel(rootParent)
+			.SetText('If a player loses a starting territory, he has lost');
+		end
+	else
+		UI.CreateLabel(rootParent)
+		.SetText('Cause this game runs with an old version of the Mod, no player is allowed to lose their starting regions');
+	end
 end
 
