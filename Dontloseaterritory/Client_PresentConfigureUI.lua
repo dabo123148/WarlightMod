00021388a48dddff9a5feacea992190771a3d4f0
dabo123148 <dabo123148@users.noreply.github.com>
2017-05-10 15:43:48 +0200

    bugfix

--- Dontloseaterritory/Client_PresentConfigureUI.lua
+++ Dontloseaterritory/Client_PresentConfigureUI.lua
@@ -7,6 +7,6 @@ function Client_PresentConfigureUI(rootParent)
     
     local horz = UI.CreateHorizontalLayoutGroup(rootParent);
   	  InputNoTerritory = UI.CreateCheckBox(horz)
-		.SetText('No Territory. If it is set to false, a player has lost if he loses a starting region else, he has lost, when he lost any territory');
+		.SetText('No Territory. If it is set to false, a player has lost if he loses a starting region else, he has lost, when he lost any territory')
 		.SetIsChecked(initialValue);
 end
