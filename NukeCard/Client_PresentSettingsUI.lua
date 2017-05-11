6781f6cc32f6fe642e43308d87cbf0185c813ac0
dabo123148 <dabo123148@users.noreply.github.com>
2017-05-11 20:53:05 +0200

    improved backward compatibility

--- NukeCard/Client_PresentSettingsUI.lua
+++ NukeCard/Client_PresentSettingsUI.lua
@@ -1,9 +1,16 @@
 
 function Client_PresentSettingsUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
-	UI.CreateLabel(vert).SetText('Territorys hit by a nuke take ' .. toString((Mod.Settings.MainTerritoryDamage*100)) .. '% Damage');
-
+	if(Mod.Settings.MainTerritoryDamage ~= nil)then
+		UI.CreateLabel(vert).SetText('Territorys hit by a nuke take ' .. toString((Mod.Settings.MainTerritoryDamage*100)) .. '% Damage');
+	else
+		UI.CreateLabel(vert).SetText('Territorys hit by a nuke take 50% Damage');
+	end
 	vert = UI.CreateVerticalLayoutGroup(rootParent);
-	UI.CreateLabel(vert).SetText('Connected Territorys to a nuke take ' .. toString((Mod.Settings.ConnectedTerritoryDamage*100)) .. '% Damage');
+	if(Mod.Settings.ConnectedTerritoryDamage ~= nil)then
+		UI.CreateLabel(vert).SetText('Connected Territorys to a nuke take ' .. toString((Mod.Settings.ConnectedTerritoryDamage*100)) .. '% Damage');
+	else
+		UI.CreateLabel(vert).SetText('Connected Territorys to a nuke take 25% Damage');
+	end
 end
 
