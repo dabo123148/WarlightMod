d99853d23559c2f77ce552bdf71bbe9c13d7cc5c
dabo123148 <dabo123148@users.noreply.github.com>
2017-05-13 11:11:39 +0200

    update to Version 2

--- LimitedMultiattack/Client_PresentConfigureUI.lua
+++ LimitedMultiattack/Client_PresentConfigureUI.lua
@@ -5,11 +5,39 @@ function Client_PresentConfigureUI(rootParent)
 		initialValue = 5;
 	end
     
-    local horz = UI.CreateHorizontalLayoutGroup(rootParent);
-	UI.CreateLabel(horz).SetText('Attacks per Multiattack ');
-    InputMaxAttacks = UI.CreateNumberInputField(horz)
-		.SetSliderMinValue(1)
-		.SetSliderMaxValue(100)
-		.SetValue(initialValue);
-
+   	local horz = UI.CreateHorizontalLayoutGroup(rootParent);
+	UI.CreateLabel(horz).SetText('Attacks per Multiattack(0=Infinit) ');
+   	InputMaxAttacks = UI.CreateNumberInputField(horz).SetSliderMinValue(0).SetSliderMaxValue(100).SetValue(initialValue);
+	horz = UI.CreateHorizontalLayoutGroup(rootParent);
+	UI.CreateLabel(horz).SetText('If one of the following checkboxes is activated, then you can only use multiattacks in turns, in which you play one of the following cards. The cards will still have their old effect.');
+	horz = UI.CreateHorizontalLayoutGroup(rootParent);
+	initialValue = Mod.Settings.ReinforcementCard;
+	if initialValue == nil then
+		initialValue = false;
+	end
+	InputReinforcementCard = UI.CreateCheckBox(horz).SetText('Activate when Playing Reinforcement Card').SetIsChecked(initialValue);
+	horz = UI.CreateHorizontalLayoutGroup(rootParent);
+	initialValue = Mod.Settings.GiftCard;
+	if initialValue == nil then
+		initialValue = false;
+	end
+	InputGiftCard = UI.CreateCheckBox(horz).SetText('Activate when Playing Gift Card').SetIsChecked(initialValue);
+	horz = UI.CreateHorizontalLayoutGroup(rootParent);
+	initialValue = Mod.Settings.AirliftCard;
+	if initialValue == nil then
+		initialValue = false;
+	end
+	InputAirliftCard = UI.CreateCheckBox(horz).SetText('Activate when Playing Airlift Card').SetIsChecked(initialValue);
+	horz = UI.CreateHorizontalLayoutGroup(rootParent);
+	initialValue = Mod.Settings.ReconnaisanceCard;
+	if initialValue == nil then
+		initialValue = false;
+	end
+	InputReconnaisanceCard = UI.CreateCheckBox(horz).SetText('Activate when Playing Reconnaisance Card').SetIsChecked(initialValue);
+	horz = UI.CreateHorizontalLayoutGroup(rootParent);
+	initialValue = Mod.Settings.SpyCard;
+	if initialValue == nil then
+		initialValue = false;
+	end
+	InputSpyCard = UI.CreateCheckBox(horz).SetText('Activate when Playing Spy Card').SetIsChecked(initialValue);
 end
