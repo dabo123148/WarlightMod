88002334b299e760484b1c6033fa155aa5455c20
dabo123148 <dabo123148@users.noreply.github.com>
2017-05-14 08:42:30 +0200

    bugfix

--- LimitedMultiattack/Client_SaveConfigureUI.lua
+++ LimitedMultiattack/Client_SaveConfigureUI.lua
@@ -15,31 +15,41 @@ function Client_SaveConfigureUI(alert)
 	if( Mod.Settings.ReinforcementCard == nil)then
 		Mod.Settings.ReinforcementCard = false;
 	else
-		boundtocards=true;
+		if(Mod.Settings.ReinforcementCard)then
+			boundtocards=true;
+		end
 	end
 	Mod.Settings.GiftCard = InputGiftCard.GetIsChecked();
 	if( Mod.Settings.GiftCard == nil)then
 		Mod.Settings.GiftCard = false;
 	else
-		boundtocards=true;
+		if(Mod.Settings.GiftCard)then
+			boundtocards=true;
+		end
 	end
 	Mod.Settings.AirliftCard = InputAirliftCard.GetIsChecked();
 	if( Mod.Settings.AirliftCard == nil)then
 		Mod.Settings.AirliftCard = false;
 	else
-		boundtocards=true;
+		if(Mod.Settings.AirliftCard)then
+			boundtocards=true;
+		end
 	end
 	Mod.Settings.ReconnaisanceCard = InputReconnaisanceCard.GetIsChecked();
 	if( Mod.Settings.ReconnaisanceCard == nil)then
 		Mod.Settings.ReconnaisanceCard = false;
 	else
-		boundtocards=true;
+		if(Mod.Settings.ReconnaisanceCard)then
+			boundtocards=true;
+		end
 	end
 	Mod.Settings.SpyCard = InputSpyCard.GetIsChecked();
 	if( Mod.Settings.SpyCard == nil)then
 		Mod.Settings.SpyCard = false;
 	else
-		boundtocards=true;
+		if(Mod.Settings.SpyCard)then
+			boundtocards=true;
+		end
 	end
 	if(boundtocards == false)then
 		if(Mod.Settings.MaxAttacks==0)then
