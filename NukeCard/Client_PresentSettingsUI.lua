08b5eb1fb8aefde5537a11ffeed85ecee873a6e9
dabo123148 <dabo123148@users.noreply.github.com>
2017-05-16 08:16:46 +0200

    try

--- NukeCard/Client_PresentSettingsUI.lua
+++ NukeCard/Client_PresentSettingsUI.lua
@@ -2,7 +2,7 @@
 function Client_PresentSettingsUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
 	if(Mod.Settings.MainTerritoryDamage ~= nil)then
-		UI.CreateLabel(vert).SetText('Territorys hit by a nuke take ' .. toString((Mod.Settings.MainTerritoryDamage*100)) .. '% Damage');
+		UI.CreateLabel(vert).SetText('Territorys hit by a nuke take ' .. (Mod.Settings.MainTerritoryDamage*100) .. '% Damage');
 	else
 		UI.CreateLabel(vert).SetText('Territorys hit by a nuke take 50% Damage');
 	end
