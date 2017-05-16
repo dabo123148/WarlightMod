1036d3dd72b9e1ae48294cc948a90dd223ee331d
dabo123148 <dabo123148@users.noreply.github.com>
2017-05-16 08:22:26 +0200

    update to Version 2.1

--- NukeCard/Client_PresentConfigureUI.lua
+++ NukeCard/Client_PresentConfigureUI.lua
@@ -1,26 +1,26 @@
 
 function Client_PresentConfigureUI(rootParent)
 	local initialValue1 = Mod.Settings.MainTerritoryDamage;
-	if initialValue1 == nil then initialValue1 = 0.5; end
+	if initialValue1 == nil then initialValue1 = 50; end
     
 
     local horz1 = UI.CreateHorizontalLayoutGroup(rootParent);
-	UI.CreateLabel(horz1).SetText('Main Territory Damage in % (0 = 0% 1= 100%)');
+	UI.CreateLabel(horz1).SetText('Main Territory Damage in %');
     inputMainTerritoryDamage = UI.CreateNumberInputField(horz1)
 		.SetSliderMinValue(0)
-		.SetSliderMaxValue(1)
+		.SetSliderMaxValue(100)
 		.SetValue(initialValue1)
-		.SetWholeNumbers(false);
+		.SetWholeNumbers(true);
 
 	local initialValue2 = Mod.Settings.ConnectedTerritoryDamage;
-	if initialValue2 == nil then initialValue2 = 0.25; end
+	if initialValue2 == nil then initialValue2 = 25; end
 
 	horz1 = UI.CreateHorizontalLayoutGroup(rootParent);
-	UI.CreateLabel(horz1).SetText('Connected Territory Damage in % (0 = 0% 1= 100%)');
+	UI.CreateLabel(horz1).SetText('Connected Territory Damage in %');
     inputConnectedTerritoryDamage = UI.CreateNumberInputField(horz1)
 		.SetSliderMinValue(0)
-		.SetSliderMaxValue(1)
+		.SetSliderMaxValue(100)
 		.SetValue(initialValue2)
-		.SetWholeNumbers(false);
+		.SetWholeNumbers(true);
 
 end
