1ac5d0da33030bc31908dd12d812e5bf338a4e1c
dabo123148 <dabo123148@users.noreply.github.com>
2017-05-16 08:21:17 +0200

    update to Version 2.1

--- NukeCard/Client_PresentSettingsUI.lua
+++ NukeCard/Client_PresentSettingsUI.lua
@@ -2,13 +2,13 @@
 function Client_PresentSettingsUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
 	if(Mod.Settings.MainTerritoryDamage ~= nil)then
-		UI.CreateLabel(vert).SetText('Territorys hit by a nuke take ' .. (Mod.Settings.MainTerritoryDamage*100) .. '% Damage');
+		UI.CreateLabel(vert).SetText('Territorys hit by a nuke take ' .. (Mod.Settings.MainTerritoryDamage) .. '% Damage');
 	else
 		UI.CreateLabel(vert).SetText('Territorys hit by a nuke take 50% Damage');
 	end
 	vert = UI.CreateVerticalLayoutGroup(rootParent);
 	if(Mod.Settings.ConnectedTerritoryDamage ~= nil)then
-		UI.CreateLabel(vert).SetText('Connected Territorys to a nuke take ' .. (Mod.Settings.ConnectedTerritoryDamage*100) .. '% Damage');
+		UI.CreateLabel(vert).SetText('Connected Territorys to a nuke take ' .. (Mod.Settings.ConnectedTerritoryDamage) .. '% Damage');
 	else
 		UI.CreateLabel(vert).SetText('Connected Territorys to a nuke take 25% Damage');
 	end
