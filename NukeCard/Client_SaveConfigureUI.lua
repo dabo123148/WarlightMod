7f79847b27b58b0a5e1f2f2c1f1c9c36a327f00d
dabo123148 <dabo123148@users.noreply.github.com>
2017-05-16 08:23:12 +0200

    update to Version 2.1

--- NukeCard/Client_SaveConfigureUI.lua
+++ NukeCard/Client_SaveConfigureUI.lua
@@ -2,7 +2,7 @@
 function Client_SaveConfigureUI(alert)
     Mod.Settings.MainTerritoryDamage = inputMainTerritoryDamage.GetValue();
 	Mod.Settings.ConnectedTerritoryDamage = inputConnectedTerritoryDamage.GetValue();
-	if(Mod.Settings.MainTerritoryDamage > 1 or Mod.Settings.ConnectedTerritoryDamage>1)then
+	if(Mod.Settings.MainTerritoryDamage > 100 or Mod.Settings.ConnectedTerritoryDamage>100)then
 		alert('More than 100% Damage is impossible')
 	end
 	if(Mod.Settings.MainTerritoryDamage < 0 or Mod.Settings.ConnectedTerritoryDamage<0)then
