8241bb5b3dd1676a262897f4b80938f39132304b
dabo123148 <dabo123148@users.noreply.github.com>
2017-05-20 15:16:14 +0200

    update to Version 3.0

--- NukeCard/Server_AdvanceTurn.lua
+++ NukeCard/Server_AdvanceTurn.lua
@@ -4,21 +4,37 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 		local Effect = {};
 		for _,conn in pairs(game.Map.Territories[order.TargetTerritory].ConnectedTo) do
 			Effect[tablelength(Effect)+1] = WL.TerritoryModification.Create(conn.ID);
-			if(Mod.Settings.ConnectedTerritoryDamage ~= nil)then
-				if(Mod.Settings.ConnectedTerritoryDamage ~= 0)then
-					Effect[tablelength(Effect)].SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[conn.ID].NumArmies.NumArmies*(1-(Mod.Settings.ConnectedTerritoryDamage/100));
+			if(Mod.Settings.Friendlyfire ~= nil)then
+				if(game.ServerGame.LatestTurnStanding.Territories[conn.ID].OwnerPlayerID ~= order.PlayerID or Mod.Settings.Friendlyfire == false)then
+					if(Mod.Settings.ConnectedTerritoryDamage ~= 0)then
+						Effect[tablelength(Effect)].SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[conn.ID].NumArmies.NumArmies*(1-(Mod.Settings.ConnectedTerritoryDamage/100));
+					end
 				end
 			else
-				Effect[tablelength(Effect)].SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[conn.ID].NumArmies.NumArmies*0.75;
+				if(Mod.Settings.ConnectedTerritoryDamage ~= nil)then
+					if(Mod.Settings.ConnectedTerritoryDamage ~= 0)then
+						Effect[tablelength(Effect)].SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[conn.ID].NumArmies.NumArmies*(1-(Mod.Settings.ConnectedTerritoryDamage/100));
+					end
+				else
+					Effect[tablelength(Effect)].SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[conn.ID].NumArmies.NumArmies*0.75;
+				end
 			end
 		end
 		Effect[tablelength(Effect)+1] = WL.TerritoryModification.Create(order.TargetTerritory);
-		if(Mod.Settings.MainTerritoryDamage ~= nil)then
-			if(Mod.Settings.MainTerritoryDamage ~= 0)then
-				Effect[tablelength(Effect)].SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[order.TargetTerritory].NumArmies.NumArmies*(1-(Mod.Settings.MainTerritoryDamage/100));
+		if(Mod.Settings.Friendlyfire ~= nil)then
+			if(game.ServerGame.LatestTurnStanding.Territories[order.TargetTerritory].OwnerPlayerID ~= order.PlayerID or Mod.Settings.Friendlyfire == false)then
+				if(Mod.Settings.MainTerritoryDamage ~= 0)then
+					Effect[tablelength(Effect)].SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[order.TargetTerritory].NumArmies.NumArmies*(1-(Mod.Settings.MainTerritoryDamage/100));
+				end
 			end
 		else
-			Effect[tablelength(Effect)].SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[order.TargetTerritory].NumArmies.NumArmies/2;
+			if(Mod.Settings.MainTerritoryDamage ~= nil)then
+				if(Mod.Settings.MainTerritoryDamage ~= 0)then
+					Effect[tablelength(Effect)].SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[order.TargetTerritory].NumArmies.NumArmies*(1-(Mod.Settings.MainTerritoryDamage/100));
+				end
+			else
+				Effect[tablelength(Effect)].SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[order.TargetTerritory].NumArmies.NumArmies/2;
+			end
 		end
 		addNewOrder(WL.GameOrderEvent.Create(order.PlayerID,'Nuked ' .. game.Map.Territories[order.TargetTerritory].Name,{},Effect));
 	end
