dbe8178ac8c946be7d9dfd93f70aeb15e03c7036
dabo123148 <dabo123148@users.noreply.github.com>
2017-05-20 18:09:38 +0200

    try

--- OneWayConnections/Client_PresentConfigureUI.lua
+++ OneWayConnections/Client_PresentConfigureUI.lua
@@ -17,7 +17,7 @@ function Client_PresentConfigureUI(rootParent)
 		 if(enteredtext2 == nil)then
 			 enteredtext2 = "";
 		 end
-		addnewline(enteredtext,enteredtext2);
+		--addnewline(enteredtext,enteredtext2);
 		num = num+2;
 	end
 	local buttonlayer = UI.CreateHorizontalLayoutGroup(rootParent);
