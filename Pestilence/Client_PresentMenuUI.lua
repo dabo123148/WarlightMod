1482bc468c858a51d5dcc7976b82d9fe118831e4
dabo123148 <dabo123148@users.noreply.github.com>
2017-05-21 20:01:20 +0200

    auto deployer

---
+++ Pestilence/Client_PresentMenuUI.lua
@@ -0,0 +1,28 @@
+function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game)
+	Game = game;
+	setMaxSize(450, 250);
+	vert = UI.CreateVerticalLayoutGroup(rootParent);
+	if (game.Us == nil) then
+		UI.CreateLabel(vert).SetText("You cannot use the Auto Deployer cause you aren't in the game");
+		return;
+	end
+	local horz = UI.CreateHorizontalLayoutGroup(vert);
+	UI.CreateLabel(horz).SetText("Auto Deployer");
+	makeorderbutton = UI.CreateButton(horz).SetText("Run").SetOnClick(makeorders);
+end
+function makeorders()
+	local orders = Game.Orders;
+	local myID = Game.Us.ID;
+	for _, terr in pairs(Game.ServerGame.LatestTurnStanding.Territories)do
+		if(terr.OwnerPlayerID == myID)then
+			table.insert(orders, WL.GameOrderDeploy.Create(myID, Mod.Settings.PestilenceStrength, terr.ID));
+		end
+	end
+end
+function  tablelength(T)
+	local count = 0;
+	for _, elem in pairs(T)do
+		count = count + 1;
+	end
+	return count;
+end
