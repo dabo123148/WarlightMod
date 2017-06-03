8508f249f064510d8d86c0ef4330687526082996
RealMelwei <melvin@weissprivat.de>
2017-06-03 11:49:09 +0200

    Update

--- Custom_Card_Package/Client_PresentConfigureUI.lua
+++ Custom_Card_Package/Client_PresentConfigureUI.lua
@@ -1,13 +1,11 @@
 
 function Client_PresentConfigureUI(rootParent)
-	local initialValue1 = Mod.Settings.PestilenceStrength;
-	if initialValue1 == nil then initialValue1 = 1; end
     
 
     local horz1 = UI.CreateHorizontalLayoutGroup(rootParent);
 	UI.CreateLabel(horz1).SetText('Pestilence Card');
-	PestCardCheckbox=horz1.CreateCheckBox;
-	PestCardCheckbox.OnValueChanged=PestCardCheckBoxChanged();
+	PestCardCheckbox=UI.CreateCheckBox(horz1);
+	PestCardCheckbox.SetOnValueChanged(PestCardCheckBoxChanged);
 
 end
 
