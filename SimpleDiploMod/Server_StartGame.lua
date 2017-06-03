f0f84dbefbf5f7de544482b12bac5fbf82c18279
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-03 15:11:21 +0200

    update

--- SimpleDiploMod/Server_StartGame.lua
+++ SimpleDiploMod/Server_StartGame.lua
@@ -2,7 +2,6 @@ function Server_StartGame(game,standing)
    local playerGameData = Mod.PlayerGameData;
    for _,terr in pairs(standing.Territories)do
       if(terr.OwnerPlayerID > 50)then
-         print(Mod.Settings.StartMoney);
          playerGameData[terr.OwnerPlayerID] ={ Money=tostring(Mod.Settings.StartMoney)};
       end
    end
