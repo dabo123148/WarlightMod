6fd1270a7aa3c90edd295487e89e46ea58331a92
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-03 10:23:16 +0200

    update

--- SimpleDiploMod/Client_PresentConfigureUI.lua
+++ SimpleDiploMod/Client_PresentConfigureUI.lua
@@ -12,6 +12,10 @@ function Client_PresentConfigureUI(rootParent)
 	if(PublicAlliesinit == nil)then
 		PublicAlliesinit = true;
 	end
+	local StartMoneyinit = Mod.Settings.StartMoney;
+	if(StartMoneyinit == nil)then
+		StartMoneyinit = 100;
+	end
 	texthorz = UI.CreateHorizontalLayoutGroup(rootParent);
 	UI.CreateLabel(texthorz).SetText('AI Settings');
    	local horz = UI.CreateHorizontalLayoutGroup(rootParent);
@@ -22,4 +26,8 @@ function Client_PresentConfigureUI(rootParent)
 	SeeAllyTerritoriesCheckbox = UI.CreateCheckBox(horz).SetText('Allow Players to see the territories of their allies').SetIsChecked(SeeAllyTerritoriesinit);
 	horz = UI.CreateHorizontalLayoutGroup(rootParent);
 	PublicAlliesCheckbox = UI.CreateCheckBox(horz).SetText('Allow everyone to see every ally').SetIsChecked(PublicAlliesinit);
+	texthorz = UI.CreateHorizontalLayoutGroup(rootParent);
+	UI.CreateLabel(texthorz).SetText('Shop System');
+	texthorz = UI.CreateHorizontalLayoutGroup(rootParent);
+	inputStartMoney = UI.CreateNumberInputField(horz1).SetSliderMinValue(0).SetSliderMaxValue(100).SetText('Starting Money').SetValue(StartMoneyinit);
 end
