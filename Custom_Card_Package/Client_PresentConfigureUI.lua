7aee0927985cb73ffe4c97e3316227c6d0314d1e
RealMelwei <melvin@weissprivat.de>
2017-06-03 11:54:21 +0200

    Update Client_PresentConfigureUI.lua

--- Custom_Card_Package/Client_PresentConfigureUI.lua
+++ Custom_Card_Package/Client_PresentConfigureUI.lua
@@ -1,11 +1,10 @@
 
 function Client_PresentConfigureUI(rootParent)
-    
+    local PestCardIn=false;
 
     local horz1 = UI.CreateHorizontalLayoutGroup(rootParent);
 	UI.CreateLabel(horz1).SetText('Pestilence Card');
-	PestCardCheckbox=UI.CreateCheckBox(horz1);
-	PestCardCheckbox.SetOnValueChanged(PestCardCheckBoxChanged);
+	PestCardCheckbox=UI.CreateCheckBox(horz1).SetText('Include Pestilence Card').SetIsChecked(PestCardIn).SetOnValueChanged(PestCardCheckBoxChanged);
 
 end
 
