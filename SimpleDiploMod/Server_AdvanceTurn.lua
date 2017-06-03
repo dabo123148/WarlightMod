e85ed4a87daa075217363106d087f54c404092dc
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-03 06:40:47 +0200

    updatetest

--- SimpleDiploMod/Server_AdvanceTurn.lua
+++ SimpleDiploMod/Server_AdvanceTurn.lua
@@ -1,10 +1,10 @@
 function Server_AdvanceTurn_Start (game,addNewOrder)
 	AllAIs = {};
 	for _,terr in pairs(game.ServerGame.LatestTurnStanding.Territories)do
-		local Match = false;
 		local CheckingID = terr.OwnerPlayerID;
 		if(CheckingID ~= WL.PlayerID.Neutral)then
 			if(game.Game.Players[CheckingID].IsAI)then
+				local Match = false;
 				for _,knownAIs in pairs(AllAIs)do
 					if(CheckingID == knownAIs)then
 						Match = true;
@@ -31,113 +31,172 @@ function Server_AdvanceTurn_Start (game,addNewOrder)
 			end
 		end
 	end
+	Attacksbetween = {};
 end
 function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
 	if(order.proxyType == "GameOrderAttackTransfer")then
 		if(result.IsAttack and game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID ~= WL.PlayerID.Neutral)then
-			if(InWar(game.ServerGame.LatestTurnStanding.Territories[order.From].OwnerPlayerID,game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID) == true)then
+			if(InWar(order.PlayerID,game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID) == true)then
 				--Set attacks between
+				if(Attacksbetween[order.PlayerID] ~= nil)then
+					bereitsgemachteangriffe = Attacksbetween[order.PlayerID];
+					if(bereitsgemachteangriffe == nil)then
+						bereitsgemachteangriffe={};
+					end
+					bereitsgemachteangriffe[game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID] = true;
+					Attacksbetween[order.PlayerID] = bereitsgemachteangriffe;
+				else
+					Attacksbetween[order.PlayerID] = {};
+					Attacksbetween[order.PlayerID][game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID] = true;
+				end
+				if(Attacksbetween[game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID] ~= nil)then
+					bereitsgemachteangriffe = Attacksbetween[game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID];
+					if(bereitsgemachteangriffe == nil)then
+						bereitsgemachteangriffe={};
+					end
+					bereitsgemachteangriffe[order.PlayerID].OwnerPlayerID] = true;
+					Attacksbetween[game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID] = bereitsgemachteangriffe;
+				else
+					Attacksbetween[game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID] = {};
+					Attacksbetween[game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID][order.PlayerID] = true;
+				end
 			else
 				skipThisOrder(WL.ModOrderControl.Skip);
 				--War declaration
 				if(Mod.Settings.AllowAIDeclaration == false)then
 					local Match = false;
 					for _, AI in pairs(AllAIs)do
-						if(game.ServerGame.LatestTurnStanding.Territories[order.From].OwnerPlayerID == AI)then
+						if(order.PlayerID == AI)then
 							Match = true;
 						end
 					end
 					if(Match == false)then
-						DeclearWar(game.ServerGame.LatestTurnStanding.Territories[order.From].OwnerPlayerID,game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID);
+						DeclearWar(order.PlayerID,game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID);
 						--Allys declear war on order.From if not allied with order.From
 					end
 				else
-					DeclearWar(game.ServerGame.LatestTurnStanding.Territories[order.From].OwnerPlayerID,game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID);
+					DeclearWar(order.PlayerID,game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID);
 					--Allys declear war on order.From if not allied with order.From
 				end
 			end
 		end
 	end
-	--War declearation through sanction card
-	if(order.proxyType == "GameOrderPlayCardSanctions")then
-		print('Sanction');
-		skipThisOrder(WL.ModOrderControl.Skip);
-		if(InWar(order.PlayerID,order.SanctionedPlayerID) == false)then
-			if(Mod.Settings.AllowAIDeclaration == false)then
-				local Match = false;
-				if(AllAIs == nil)then
-					print('Fehler');
+	if(order.proxyType == "GameOrderCustom")then
+		if(check(order.Message,"Declared war on"))then
+			local with = toint(order.Message);
+			if(InWar(order.PlayerID,order.SanctionedPlayerID) == false)then
+				local ordersplit = stringtochararray(order.Message);
+				local with = "";
+				local num = 16;
+				while(num < tablelength(ordersplit))do
+					with = with + splitelem[num];
+					num = num + 1;
 				end
-				for _, AI in pairs(AllAIs)do
-					if(order.PlayerID == AI)then
-						Match = true;
-					end
-				end
-				if(Match == false)then
-					DeclearWar(order.PlayerID,order.SanctionedPlayerID);
-					--Allys declear war on order.PlayerID if not allied with order.PlayerID
-				end
-			else
-				DeclearWar(order.PlayerID,order.SanctionedPlayerID);
-				--Allys declear war on order.PlayerID if not allied with order.PlayerID
+				print('with ' .. with);
+				DeclearWar(order.PlayerID,tonumber(with));
 			end
-		else
-			--peace offer, but Allys are still in war
 		end
-	end
-	--Ally offer & Peace declearation
-	if(order.proxyType == "GameOrderPlayCardSpy")then
-		
+		if(check(order.Message,"Offer peace to"))then
+			
+		end
+		if(check(order.Message,"Offer ally to"))then
+			
+		end
+		if(check(order.Message,"Removed ally with"))then
+			
+		end
 	end
 end
 function Server_AdvanceTurn_End (game,addNewOrder)
 	--add new Allys
 	--add new war decleartions
 	if(RemainingDeclerations ~= nil)then
-		print('T5');
 		for _,newwar in pairs(RemainingDeclerations)do
 			local P1 = tonumber(stringtotable(newwar)[1]);
 			local P2 = tonumber(stringtotable(newwar)[2]);
-			local newinwar = {};
-			if(War[P1] ~= nil)then
+			local publicGameData = Mod.PublicGameData;
+			if(Mod.PublicGameData.War[P1] ~= nil)then
+				local newinwar = {};
 				for _, alreadyinwar in pairs(War[P1])do
 					newinwar[alreadyinwar] = true;
 				end
+				publicGameData.War[P1] = newinwar;
+			else
+				publicGameData.War[P1] = {};
 			end
-			newinwar[P2] = true;
-			War[P1] = newinwar;
+			publicGameData.War[P1][P2] = true;
 			--addNewOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral, "The Player with the player ID " .. tostring(P1) .. " decleared war on " .. tostring(P2), nil,{}));
-			addNewOrder(WL.GameOrderCustom.Create(P1, "Declears war on " .. tostring(P2), "War " .. tostring(P1) .. " " .. tostring(P2)));
+			--addNewOrder(WL.GameOrderCustom.Create(P1, "Declars war on " .. tostring(P2), ""));
 			local P3 = P2;
 			P2 = P1;
-			p1 = P3;
-			local newinwar = {};
-			if(War[P1] ~= nil)then
-				for _, alreadyinwar in pairs(War[P1])do
+			P1 = P3;
+			if(Mod.PublicGameData.War[P1] ~= nil)then
+				local newinwar ={};
+				for _, alreadyinwar in pairs(Mod.PublicGameData.War[P1])do
 					newinwar[alreadyinwar] = true;
 				end
+				publicGameData.War[P1] = newinwar;
+			else
+				publicGameData.War[P1] = {};
 			end
-			newinwar[P2] = true;
-			War[P1] = newinwar;
+			publicGameData.War[P1][P2] = true;
+			publicGameData.War[P1] = true;
+			Mod.PublicGameData = publicGameData;
 		end
 	end
 	RemainingDeclerations = {};
-	if(Mod.Settings.SeeAllyTerritories)then
-		--play on every ally a reconnaisance card
-		for _, player in pairs(AllPlayerIDs)do
-			for _, terr in pairs(game.ServerGame.LatestTurnStanding.Territories)do
-				if(IsAlly(player,terr.OwnerPlayerID))then
-					addNewOrder(WL.GameOrderPlayCardReconnaissance.Create(WL.NoParameterCardInstance.Create(100, WL.CardID.Reconnaissance), player, terr));
+	if(RemainingAllies ~= nil)then
+		for _,newwar in pairs(RemainingAllies)do
+			local P1 = tonumber(stringtotable(newwar)[1]);
+			local P2 = tonumber(stringtotable(newwar)[2]);
+			local publicGameData = Mod.PublicGameData;
+			if(Mod.PublicGameData.Ally[P1] ~= nil)then
+				local newinwar = {};
+				for _, alreadyinwar in pairs(Ally[P1])do
+					newinwar[alreadyinwar] = true;
 				end
+				publicGameData.Ally[P1] = newinwar;
+			else
+				publicGameData.Ally[P1] = {};
 			end
+			publicGameData.Ally[P1][P2] = true;
+			local P3 = P2;
+			P2 = P1;
+			P1 = P3;
+			if(Mod.PublicGameData.Ally[P1] ~= nil)then
+				local newinwar ={};
+				for _, alreadyinwar in pairs(Mod.PublicGameData.Ally[P1])do
+					newinwar[alreadyinwar] = true;
+				end
+				publicGameData.Ally[P1] = newinwar;
+			else
+				publicGameData.Ally[P1] = {};
+			end
+			publicGameData.Ally[P1][P2] = true;
+			publicGameData.Ally[P1] = true;
+			Mod.PublicGameData = publicGameData;
 		end
 	end
+	RemainingAllies = {};
+	--if(Mod.Settings.SeeAllyTerritories)then
+		--play on every ally a reconnaisance card
+		--for _, player in pairs(AllPlayerIDs)do
+			--for _, terr in pairs(game.ServerGame.LatestTurnStanding.Territories)do
+				--if(IsAlly(player,terr.OwnerPlayerID))then
+					--addNewOrder(WL.GameOrderPlayCardReconnaissance.Create(WL.NoParameterCardInstance.Create(100, WL.CardID.Reconnaissance), player, terr));
+				--end
+			--end
+		--end
+	--end
 end
 function RemoveAlly(Player1,Player2)
+	
 end
 function RemoveWar(Player1,Player2)
+	
 end
 function DeclearWar(Player1,Player2)
+	--Allys declear war on order.PlayerID if not allied with order.PlayerID
 	if(IsAlly(Player1,Player2)==false)then
 		if(RemainingDeclerations == nil)then
 			RemainingDeclerations = {};
@@ -161,13 +220,15 @@ function DeclearWar(Player1,Player2)
 	end
 end
 function InWar(Player1,Player2)
-	if(War == nil)then
+	if(Mod.PublicGameData.War == nil)then
 		print('neu gesetzt');
-		War = {{}};
+		local publicGameData = Mod.PublicGameData;
+ 		publicGameData.War={};
+		Mod.PublicGameData=publicGameData;
 	end
-	if(War[Player1] ~= nil)then
+	if(Mod.PublicGameData.War[Player1] ~= nil)then
 		print('Test');
-		if(War[Player1][Player2])then
+		if(Mod.PublicGameData.War[Player1][Player2])then
 			print('Test2');
 			return true;
 		end
@@ -186,6 +247,14 @@ function tablelength(T)
 		count = count + 1;
 	end
 	return count;
+end
+function stringtochararray(variable)
+	chartable = {};
+	while(string.len(variable)>0)do
+		chartable[tablelength(chartable)] = string.sub(variable, 1 , 1);
+		variable = string.sub(variable, 2);
+	end
+	return chartable;
 end
 function stringtotable(variable)
 	chartable = {};
@@ -215,4 +284,17 @@ function stringtotable(variable)
 		end
 	end
 	return newtable;
+end
+function check(message,variable)
+	local match = false;
+	local mess = stringtochararray(message);
+	local varchararray = stringtochararray(variable);
+	local num = 0;
+	while(mess[num] ~= nil)then
+		if(mess[num] != varchararray[num])then
+			match = false;
+		end
+		num += 1;
+	end
+	return match;
 end
