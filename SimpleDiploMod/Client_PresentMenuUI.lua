90b8e480a79108639661adbd8fc4ca63099a3ade
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-03 10:40:03 +0200

    bugfix

--- SimpleDiploMod/Client_PresentMenuUI.lua
+++ SimpleDiploMod/Client_PresentMenuUI.lua
@@ -6,24 +6,23 @@ function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game)
 		UI.CreateLabel(vert).SetText("You cannot use the Diplomacy game, cause you aren't in the game");
 		return;
 	end
-  local playerGameData = Mod.PlayerGameData;
-  Mod.PlayerGameData = playerGameData;
-  local horz = UI.CreateHorizontalLayoutGroup(vert);
-  UI.CreateLabel(horz).SetText(Mod.PlayerGameData[game.Us].Money);
+	local playerGameData = Mod.PlayerGameData;
+  	local horz = UI.CreateHorizontalLayoutGroup(vert);
+ 	 UI.CreateLabel(horz).SetText(Mod.PlayerGameData[game.Us].Money);
 	local horz = UI.CreateHorizontalLayoutGroup(vert);
 	UI.CreateLabel(horz).SetText("Shop");
 	openshopbutton = UI.CreateButton(horz).SetText("Shop").SetOnClick(Openshop);
-  local horz = UI.CreateHorizontalLayoutGroup(vert);
-  UI.CreateLabel(horz).SetText("Declar War");
+ 	local horz = UI.CreateHorizontalLayoutGroup(vert);
+ 	UI.CreateLabel(horz).SetText("Declar War");
 	declarewarbutton = UI.CreateButton(horz).SetText("Declar War").SetOnClick(OpenDeclarWar);
-  local horz = UI.CreateHorizontalLayoutGroup(vert);
-  UI.CreateLabel(horz).SetText("Auto Deployer");
+ 	local horz = UI.CreateHorizontalLayoutGroup(vert);
+	UI.CreateLabel(horz).SetText("Auto Deployer");
 	offerpeacebutton = UI.CreateButton(horz).SetText("Offer Peace").SetOnClick(OpenOfferPeace);
-  local horz = UI.CreateHorizontalLayoutGroup(vert);
-  UI.CreateLabel(horz).SetText("Offer Allianze");
+	local horz = UI.CreateHorizontalLayoutGroup(vert);
+	UI.CreateLabel(horz).SetText("Offer Allianze");
 	offerallianzebutton = UI.CreateButton(horz).SetText("Offer Allianze").SetOnClick(OpenOfferAllianze);
-  local horz = UI.CreateHorizontalLayoutGroup(vert);
-  UI.CreateLabel(horz).SetText("Pending Requests");
+  	local horz = UI.CreateHorizontalLayoutGroup(vert);
+  	UI.CreateLabel(horz).SetText("Pending Requests");
 	offerallianzebutton = UI.CreateButton(horz).SetText("Pending Requests").SetOnClick(OpenPendingRequests);
 end
 function Openshop()
