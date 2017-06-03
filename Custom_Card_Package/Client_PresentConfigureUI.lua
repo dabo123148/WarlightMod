1798300eef536657da5888ed578b94c86905c640
RealMelwei <melvin@weissprivat.de>
2017-06-03 19:55:50 +0200

    Update Client_PresentConfigureUI.lua

--- Custom_Card_Package/Client_PresentConfigureUI.lua
+++ Custom_Card_Package/Client_PresentConfigureUI.lua
@@ -3,10 +3,20 @@ function Client_PresentConfigureUI(rootParent)
     root=rootParent;
     
     PestCardIn=false;
+    Mod.Settings.PestCardIn=false;
+	
     PestCardStrength=1;
+    Mod.Settings.PestCardStrength=1;
     PestCardStrengthSlider=nil;
+	
     PestCardPiecesNeeded=11;
+    Mod.Settings.PestCardPiecesNeeded=11;
     PestCardPiecesNeededBox=nil;
+	
+    PestCardStartPieces=0;
+    Mod.Settings.PestCardStartPieces=0;
+    PestCardStartPiecesBox=nil;
+    
     vertPest = UI.CreateVerticalLayoutGroup(rootParent);
     PestCardCheckbox=UI.CreateCheckBox(vertPest).SetText('Include Pestilence Card').SetIsChecked(PestCardIn).SetOnValueChanged(PestCardCheckBoxChanged);
     
@@ -17,8 +27,11 @@ function PestCardCheckBoxChanged()
     if(PestCardIn) then
         PestCardStrengthSlider=UI.CreateNumberInputField(vertPest).SetSliderMinValue(1).SetSliderMaxValue(3).SetValue(PestCardStrength);
 	PestCardPiecesNeededBox=UI.CreateNumberInputField(vertPest).SetSliderMinValue(1).SetSliderMaxValue(20).SetValue(PestCardPiecesNeeded);
+	PestCardStartPiecesBox=UI.CreateNumberInputField(vertPest).SetSliderMinValue(0).SetSliderMaxValue(20).SetValue(PestCardStartPieces);
+	
     else
 	UI.Destroy(PestCardStrengthSlider);
 	UI.Destroy(PestCardPiecesNeededBox);
+	UI.Destroy(PestCardStartPiecesBox);
     end
 end
