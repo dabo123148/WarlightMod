9eadb3242a176ae00e8a71a229cdf30cafabf4d0
RealMelwei <melvin@weissprivat.de>
2017-06-04 16:45:20 +0200

    Update Server_StartGame.lua

--- Custom_Card_Package/Server_StartGame.lua
+++ Custom_Card_Package/Server_StartGame.lua
@@ -2,7 +2,7 @@ function Server_StartGame(game,standing)
   local playerGameData = Mod.PlayerGameData;
   for playerID in pairs(game.ServerGame.Game.PlayingPlayers) do
     if(playerID>50)then
-      playerGameData[playerID]={ PestCardPieces=Mod.Settings.PestCardStartPieces, PestCards=0, SuccessfullyAttacked=0 };
+      playerGameData[playerID]={ PestCardPieces=Mod.Settings.PestCardStartPieces%Mod.Settings.PestCardPiecesNeeded, PestCards=(Mod.Settings.PestCardStartPieces-(Mod.Settings.PestCardStartPieces%Mod.Settings.PestCardPiecesNeeded))/PestCardPiecesNeeded, SuccessfullyAttacked=0 };
     end
   end
   Mod.PlayerGameData = playerGameData;
