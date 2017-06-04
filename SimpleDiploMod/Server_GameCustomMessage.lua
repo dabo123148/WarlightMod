a350ffce35db9c89a7bcf9455d56ee27f1858427
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-04 13:39:09 +0200

    update

--- SimpleDiploMod/Server_GameCustomMessage.lua
+++ SimpleDiploMod/Server_GameCustomMessage.lua
@@ -1,8 +1,25 @@
 function Server_GameCustomMessage(game, playerID, payload, setReturnTable)
 	print('Custom Message');
  	if(payload.Message == "Peace")then
+		local target = payload.TargetPlayerID;
+		local preis = payload.Preis;
     		print('Friedensangebot');
+		local privateGameData = Mod.PrivateGameData;
+		if(privateGameData.Peaceoffers == nil)then
+			privateGameData.Peaceoffers = {};
+			privateGameData.Peaceoffers[target] = ",";
+		else
+ 			if(privateGameData.Peaceoffers[target]==nil)then
+				privateGameData.Peaceoffers[target]=",";
+			end
+		end
+		privateGameData.Peaceoffers = privateGameData.Peaceoffers + "," + target + ","+ preis +",";
+		print(privateGameData.Peaceoffers);
+		Mod.PrivateGameData=privateGameData;
   	end
+	if(payload.Message == "Request Data")then
+		
+	end
 end
 function stringtochararray(variable)
 	chartable = {};
