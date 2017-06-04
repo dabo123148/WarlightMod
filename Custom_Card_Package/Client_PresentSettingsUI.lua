048da518efbbb2d228a04be7e52f808222c8d1e1
RealMelwei <melvin@weissprivat.de>
2017-06-04 12:04:03 +0200

    Update Client_PresentSettingsUI.lua

--- Custom_Card_Package/Client_PresentSettingsUI.lua
+++ Custom_Card_Package/Client_PresentSettingsUI.lua
@@ -1,11 +1,11 @@
 
 function Client_PresentSettingsUI(rootParent)
 	if(Mod.Settings.PestCardIn)then
-		local vertPest = UI.CreateVerticalLayoutGroup(rootParent);
-		UI.CreateLabel(vertPest).SetText('Pestilence Card Settings:');
-		UI.CreateLabel(vertPest).SetText('	Strength: ' .. Mod.Settings.PestCardStrength);
-		UI.CreateLabel(vertPest).SetText('	Card Pieces Needed: ' .. Mod.Settings.PestCardPiecesNeeded);
-		UI.CreateLabel(vertPest).SetText('	Card Pieces Given in the beginning of the game: ' .. Mod.Settings.PestCardStartPieces);
+		local vertPest1 = UI.CreateVerticalLayoutGroup(rootParent);
+		UI.CreateLabel(vertPest1).SetText('Pestilence Card Settings:');
+		UI.CreateLabel(vertPest1).SetText('	Strength: ' .. Mod.Settings.PestCardStrength);
+		UI.CreateLabel(vertPest1).SetText('	Card Pieces Needed: ' .. Mod.Settings.PestCardPiecesNeeded);
+		UI.CreateLabel(vertPest1).SetText('	Card Pieces Given in the beginning of the game: ' .. Mod.Settings.PestCardStartPieces);
 		
 	end
 end
