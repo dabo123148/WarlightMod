29851f46db85e63e1af642042d6ce4490b115d80
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-05 08:10:02 +0200

    test

--- SimpleDiploMod/Server_AdvanceTurn.lua
+++ SimpleDiploMod/Server_AdvanceTurn.lua
@@ -118,7 +118,8 @@ function Server_AdvanceTurn_End (game,addNewOrder)
 				publicGameData.War[P1] = tostring(P2) .. ",";
 			end
 			--addNewOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral, tostring(newwar), nil,{}));
-			addNewOrder(WL.GameOrderEvent.Create(P1, "Declared war on1 " .. toname(P2,game), nil,{}));
+			--addNewOrder(WL.GameOrderEvent.Create(P1, "Declared war on1 " .. toname(P2,game), nil,{}));
+			addNewOrder(WL.GameOrderEvent.Create(P1, newwar, nil,{}));
 			--addNewOrder(WL.GameOrderCustom.Create(P1, "Declars war on " .. tostring(P2), ""));
 			local P3 = P2;
 			P2 = P1;
