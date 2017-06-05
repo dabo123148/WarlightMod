eb23e2f54045e1bc1f79a8d6e1110e9c799881c7
RealMelwei <melvin@weissprivat.de>
2017-06-05 18:53:25 +0200

    Update Client_GameRefresh.lua

--- Custom_Card_Package/Client_GameRefresh.lua
+++ Custom_Card_Package/Client_GameRefresh.lua
@@ -2,15 +2,15 @@ function Client_GameRefresh(clientGame)
   if(Mod.PublicGameData.PestilenceStadium[clientGame.Us.ID]~=nil)then
     if(Mod.PublicGameData.PestilenceStadium[clientGame.Us.ID]==1)then
       if(Mod.Settings.PestCardStrength==1)then
-        UI.Alert('Somebody played a Pestilence Card on you! You will lose 1 troop on every Territory at the end of Turn '..tostring(clientGame.Game.TurnNumber+1)..'. Territories with 0 armies will be lost!';
+        UI.Alert('Somebody played a Pestilence Card on you! You will lose 1 troop on every Territory at the end of Turn '..tostring(clientGame.Game.TurnNumber+1)..'. Territories with 0 armies will be lost!');
       else
-        UI.Alert('Somebody played a Pestilence Card on you! You will lose '..tostring(Mod.Settings.PestCardStrength)..' troops on every Territory at the end of Turn '..tostring(clientGame.Game.TurnNumber+1)..'. Territories with 0 armies will be lost!';
+        UI.Alert('Somebody played a Pestilence Card on you! You will lose '..tostring(Mod.Settings.PestCardStrength)..' troops on every Territory at the end of Turn '..tostring(clientGame.Game.TurnNumber+1)..'. Territories with 0 armies will be lost!');
       end
     else
       if(Mod.Settings.PestCardStrength==2)then
-        UI.Alert('Somebody played a Pestilence Card on you! You will lose 1 troop on every Territory at the end of this turn. Territories with 0 armies will be lost!';
+        UI.Alert('Somebody played a Pestilence Card on you! You will lose 1 troop on every Territory at the end of this turn. Territories with 0 armies will be lost!');
       else
-        UI.Alert('Somebody played a Pestilence Card on you! You will lose '..tostring(Mod.Settings.PestCardStrength)..' troops on every Territory at the end of this turn. Territories with 0 armies will be lost!';
+        UI.Alert('Somebody played a Pestilence Card on you! You will lose '..tostring(Mod.Settings.PestCardStrength)..' troops on every Territory at the end of this turn. Territories with 0 armies will be lost!');
       end
     end
   end
