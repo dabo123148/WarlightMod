20999d71a99581aac8928a3e817a3353923005c4
RealMelwei <melvin@weissprivat.de>
2017-06-05 13:21:27 +0200

    Update Client_PresentMenuUI.lua

--- Custom_Card_Package/Client_PresentMenuUI.lua
+++ Custom_Card_Package/Client_PresentMenuUI.lua
@@ -1,4 +1,5 @@
 function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game,close)
+  Game=game;
   setMaxSize(450, 350);
   if(Mod.Settings.PestCardIn)then
     vertPest=UI.CreateVerticalLayoutGroup(rootParent);
@@ -11,5 +12,26 @@ function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game,close)
 end
   
 function PlayPestCard()
-    UI.Alert('TEST');
+  ClearUI();
+  vertPestCard=UI.CreateVerticalLayoutGroup(rootParent);
+  PestCardText0=UI.CreateLabel(vertPest).SetText('Select the player you want to play the Pestilence Card on: ');
+  local Pestfuncs={};
+  for playerID in pairs(Game.Game.Players) do
+    if(playerID~=Game.Us.ID)then
+      local locPlayerID=playerID;
+      Pestfuncs[playerID]=function() Pestilence(locPlayerID); end;
+      local pestPlayerButton = UI.CreateButton(vertPestCard).SetText(playerID.DisplayName(nil,false)).SetOnClick(Pestfuncs[playerID]);
+    end
+  end
+  UI.Alert('TEST');
+end
+
+function Pestilence(playerID)
+  
+end
+
+function ClearUI()
+  if(vertPest~=nil)then
+    UI.Destroy(vertPest);
+  end
 end
