f3c6ac6c3b9c8324d2ef1c59b44d8e2957800742
RealMelwei <melvin@weissprivat.de>
2017-06-05 12:18:23 +0200

    Update Client_SaveConfigureUI.lua

--- Custom_Card_Package/Client_SaveConfigureUI.lua
+++ Custom_Card_Package/Client_SaveConfigureUI.lua
@@ -1,6 +1,6 @@
 
 function Client_SaveConfigureUI(alert)
-    
+    Mod.Settings.PestCardIn=PestCardCheckbox.IsChecked();
     Mod.Settings.PestCardStrength=PestCardStrengthSlider.GetValue();
     Mod.Settings.PestCardPiecesNeeded=PestCardPiecesNeededBox.GetValue();
     Mod.Settings.PestCardStartPieces=PestCardStartPiecesBox.GetValue();
