f34ff3733246336f3538a2f5a3bd2a808cbbddfb
RealMelwei <melvin@weissprivat.de>
2017-06-05 14:35:04 +0200

    Update Server_GameCustomMessage.lua

--- Custom_Card_Package/Server_GameCustomMessage.lua
+++ Custom_Card_Package/Server_GameCustomMessage.lua
@@ -1,3 +1,6 @@
 function Server_GameCustomMessage(game,playerID,payload,setReturn)
-
+  if(payload.PestCardPlayer~=nil)then
+    PGD=Mod.PublicGameData;
+    PGD.PestilenceStadium[payload.PestCardPlayer]=1;
+  end
 end
