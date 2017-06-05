ac4b66d9b1d5d6b78b1b5809d78ad497aaeee24c
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-05 11:32:35 +0200

    bugfix

--- SimpleDiploMod/Client_PresentMenuUI.lua
+++ SimpleDiploMod/Client_PresentMenuUI.lua
@@ -111,7 +111,7 @@ function OpenPendingRequests()
 				UI.CreateLabel(horzobjlist[tablelength(horzobjlist)-1]).SetText("You haven't the money to accept this offer.");
 			else
 				local button = UI.CreateButton(horzobjlist[tablelength(horzobjlist)-1]).SetText("Accept");
-				local locNum={Knopf=button,spieler=tonumber(peacesplit[num]};
+				local locNum={Knopf=button,spieler=tonumber(peacesplit[num])};
 				AllEvilFuncs[num]=function() local InNum = locNum; AcceptPeaceOffer(InNum); end;
 				button.SetOnClick(AllEvilFuncs[num]);
 				
