abcb2094afef64f18f62617c15ad529c367609fe
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-05 14:18:19 +0200

    test

--- SimpleDiploMod/Client_PresentMenuUI.lua
+++ SimpleDiploMod/Client_PresentMenuUI.lua
@@ -126,11 +126,11 @@ function AcceptPeaceOffer(data)
 	local payload = {};
 	payload.Message = "Accept Peace";
 	payload.TargetPlayerID = data.Spieler;
-	Game.SendGameCustomMessage("Sending data...", payload, function(returnvalue)
+	--Game.SendGameCustomMessage("Sending data...", payload, function(returnvalue)
 				--if(returnvalue.Message==nil)then
-					UI.Alert("You are now again in peace with " .. toname(data.Spieler));
+	--				UI.Alert("You are now again in peace with " .. toname(data.Spieler));
 				--end
-			end);
+	--		end);
 end
 
 
