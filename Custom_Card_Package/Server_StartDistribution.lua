11cc7592f4a9cefcc5e53e6782be7da9e50cd936
RealMelwei <melvin@weissprivat.de>
2017-06-05 19:17:24 +0200

    Update Server_StartDistribution.lua

--- Custom_Card_Package/Server_StartDistribution.lua
+++ Custom_Card_Package/Server_StartDistribution.lua
@@ -4,7 +4,7 @@ function Server_StartDistribution(game,standing)
     if(playerID>50)then
       Pieces = Mod.Settings.PestCardStartPieces%Mod.Settings.PestCardPiecesNeeded;
       Cards = (Mod.Settings.PestCardStartPieces-Pieces)/Mod.Settings.PestCardPiecesNeeded;
-      playerGameData[playerID]={ PestCardPieces=Pieces, PestCards=Cards, SuccessfullyAttacked=0 };
+      playerGameData[playerID]={ PestCardPieces=0, PestCards=0, SuccessfullyAttacked=0 };
     end
   end
   local publicGameData = Mod.PublicGameData;
