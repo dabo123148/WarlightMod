e9fc5f0dd8d07dd470114e51b9439b2d6ceccbf4
RealMelwei <melvin@weissprivat.de>
2017-06-05 12:16:01 +0200

    Update Client_PresentMenuUI.lua

--- Custom_Card_Package/Client_PresentMenuUI.lua
+++ Custom_Card_Package/Client_PresentMenuUI.lua
@@ -1,6 +1,6 @@
 function Client_PresentMenuUI(rootParent)
+  setMaxSize(450, 350);
   if(Mod.Settings.PestCardIn)then
-    setMaxSize(450, 350);
     vertPest=UI.CreateVerticalLayoutGroup(rootParent);
     text1=UI.CreateLayer(vertPest).SetText('You have got '..tostring(Mod.PlayerGameData[Game.Us.ID].PestCards)..' and '..tostring(Mod.PlayerGameData[Game.Us.ID].PestCardPieces)..'/'..Mod.Settings.PestCardPiecesNeeded..' Pieces.');
   end
