c441ee053b2b4e37aabc7582f85822a2d050f597
RealMelwei <melvin@weissprivat.de>
2017-06-06 16:56:14 +0200

    Update Server_AdvanceTurn.lua

--- Custom_Card_Package/Server_AdvanceTurn.lua
+++ Custom_Card_Package/Server_AdvanceTurn.lua
@@ -17,6 +17,14 @@ function Server_AdvanceTurn_Order(game,gameOrder,result,skip,addOrder)
 			end
 		end
 	end
+	if(gameOrder.proxyType=='GameOrderCustom')then
+		if(gameOrder.Payload~=nil)then
+			if(split(gameOrder.Payload,'|')[1]=='Pestilence')then
+				PGD=Mod.PublicGameData;
+				PGD.PestilenceStadium[tonumber(split(gameOrder.Payload,'|')[2])]=1;
+			end
+		end
+	end
 end
 
 
@@ -108,4 +116,15 @@ function tablelength(T)
   local count = 0
   for _ in pairs(T) do count = count + 1 end
   return count
+end
+function split(inputstr, sep)
+        if sep == nil then
+                sep = "%s"
+        end
+        local t={} ; i=1
+        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+                t[i] = str
+                i = i + 1
+        end
+        return t
 end
