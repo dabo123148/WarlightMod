aae051f72e4d3b9549e6e5489829421a47e4128f
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-06 16:01:15 +0200

    update

--- SimpleDiploMod/Client_PresentSettingsUI.lua
+++ SimpleDiploMod/Client_PresentSettingsUI.lua
@@ -1,25 +1,26 @@
 
 function Client_PresentSettingsUI(rootParent)
 	UI.CreateLabel(rootParent).SetText('AI Settings');
-	if(Mod.Settings.AllowAIDeclaration)then
-		UI.CreateLabel(rootParent).SetText('AIs are allowed to declare war on Player');
+	UI.CreateLabel(rootParent).SetText('AIs are allowed to declare war on player : ' .. booltostring(Mod.Settings.AllowAIDeclaration));
+	UI.CreateLabel(rootParent).SetText('AIs are allowed to declare war on AIs : ' .. booltostring(Mod.Settings.AIsdeclearAIs));
+	UI.CreateLabel(rootParent).SetText('Alliance Settings');
+	UI.CreateLabel(rootParent).SetText('Allied players can see your territories : ' .. booltostring(Mod.Settings.SeeAllyTerritories));
+	UI.CreateLabel(rootParent).SetText('Allies are visible to everyone : ' .. booltostring(Mod.Settings.PublicAllies));
+	UI.CreateLabel(rootParent).SetText('Tradement System');
+	UI.CreateLabel(rootParent).SetText('Player starting money : ' .. Mod.Settings.StartMoney);
+	UI.CreateLabel(rootParent).SetText('Extra money per turn : ' .. Mod.Settings.MoneyPerTurn);
+	UI.CreateLabel(rootParent).SetText('Extra money per killed army : ' .. Mod.Settings.MoneyPerKilledArmy);
+	UI.CreateLabel(rootParent).SetText('Extra money per captured territory : ' .. Mod.Settings.MoneyPerCapturedTerritory);
+	UI.CreateLabel(rootParent).SetText('Extra money per captured bonus : ' .. Mod.Settings.MoneyPerCapturedBonus);
+	UI.CreateLabel(rootParent).SetText('Price per army : ' .. Mod.Settings.MoneyPerBoughtArmy);
+	UI.CreateLabel(rootParent).SetText('Cards that require War');
+	UI.CreateLabel(rootParent).SetText('Sanction Cards can only be played on players you are in war with : ' .. booltostring(Mod.Settings.SanctionCardRequireWar));
+	UI.CreateLabel(rootParent).SetText('Bomb Cards can only be played on players you are in war with : ' .. booltostring(Mod.Settings.BombCardRequireWar));
+end
+function booltostring(variable)
+	if(variable)then
+		return "Yes";
 	else
-		UI.CreateLabel(rootParent).SetText('AIs are not allowed to declare war on Player');
+		return "No";
 	end
-	if(Mod.Settings.AIsdeclearAIs)then
-		UI.CreateLabel(rootParent).SetText('AIs are allowed to declare war on AIs');
-	else
-		UI.CreateLabel(rootParent).SetText('AIs are not allowed to declare war AIs');
-	end
-	if(Mod.Settings.SeeAllyTerritories)then
-		UI.CreateLabel(rootParent).SetText('Allied Players can see your territories');
-	else
-		UI.CreateLabel(rootParent).SetText('Allied Players can not see your territories');
-	end
-	if(Mod.Settings.PublicAllies)then
-		UI.CreateLabel(rootParent).SetText('Allies are visible to everyone');
-	else
-		UI.CreateLabel(rootParent).SetText('Just you and your ally can see your allianze');
-	end
-	UI.CreateLabel(rootParent).SetText('Player start with ' .. Mod.Settings.StartMoney .. ' Money');
 end
