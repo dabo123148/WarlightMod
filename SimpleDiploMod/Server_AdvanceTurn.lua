1b74f0526c8b2b07b621d2c8055e91f37cae8508
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-06 17:07:19 +0200

    test

--- SimpleDiploMod/Server_AdvanceTurn.lua
+++ SimpleDiploMod/Server_AdvanceTurn.lua
@@ -45,8 +45,7 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 				if(result.IsSuccessful)then
 					local playerGameData = Mod.PlayerGameData;
 					local toowner = game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID;
-					error("Test" .. playerGameData[order.PlayerID].Money+result.AttackingArmiesKilled.NumArmies*Mod.Settings.MoneyPerKilledArmy);
-					playerGameData[order.PlayerID].Money = playerGameData[order.PlayerID].Money + result.AttackingArmiesKilled.NumArmies*Mod.Settings.MoneyPerKilledArmy;
+					playerGameData[order.PlayerID].Money = playerGameData[order.PlayerID].Money+result.AttackingArmiesKilled.NumArmies*Mod.Settings.MoneyPerKilledArmy;
 					playerGameData[toowner].Money = playerGameData[toowner].Money + result.DefendingArmiesKilled.NumArmies*Mod.Settings.MoneyPerKilledArmy;
 					Mod.PlayerGameData = playerGameData;
 				end
