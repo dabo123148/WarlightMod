a44447a898843d3352cbadac9f381c8a3a5abd20
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-09 15:32:06 +0200

    test

--- SimpleDiploMod/Client_GameRefresh.lua
+++ SimpleDiploMod/Client_GameRefresh.lua
@@ -8,7 +8,13 @@ function Client_GameRefresh(game)
 				Nachricht = Nachricht .. "\n" .. getname(NeueNachrichtensplit[num],game) .. " declared war on " .. getname(NeueNachrichtensplit[num+3],game) .. " in turn " .. NeueNachrichtensplit[num+2];
 			end
 			if(NeueNachrichtensplit[num+1] == "1")then
+				Nachricht = Nachricht .. "\n" .. getname(NeueNachrichtensplit[num],game) .. " offered " .. getname(NeueNachrichtensplit[num+3],game) .. " peace for " .. NeueNachrichtensplit[num+2] .. " turns";
+			end
+			if(NeueNachrichtensplit[num+1] == "2")then
 				Nachricht = Nachricht .. "\n" .. getname(NeueNachrichtensplit[num],game) .. " accepted the peace offer by " .. getname(NeueNachrichtensplit[num+3],game) .. " until turn " .. NeueNachrichtensplit[num+2];
+			end
+			if(NeueNachrichtensplit[num+1] == "3")then
+				Nachricht = Nachricht .. "\n" .. getname(NeueNachrichtensplit[num],game) .. " declined the peace offer off " .. getname(NeueNachrichtensplit[num+3],game);
 			end
 			num = num + 4;
 		end
