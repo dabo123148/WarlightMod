cd955a4967c32cbd3cdb0c73ed4ca510c618915d
RealMelwei <melvin@weissprivat.de>
2017-06-10 10:57:19 +0200

    Create Client_PresentMenuUI.lua

--- Custom_Card_Package/Client_PresentMenuUI.lua
+++ Custom_Card_Package/Client_PresentMenuUI.lua
@@ -79,6 +79,14 @@ function ClearUI()
   end
 end
 
+---------------------------------------------
+---------------------------------------------
+----                                     ----
+----         COMFORT FUNCTIONS           ----
+----                                     ----
+---------------------------------------------
+---------------------------------------------
+
 function Contains(array,object)
   for obj in pairs(array) do
     if(obj==object)then
@@ -95,4 +103,20 @@ function toname(playerid,game)
 		end
 	end
 	return "Error - Player ID not found. Please report to melwei[PG].";
+end
+function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
+function split(inputstr, sep)
+        if sep == nil then
+                sep = "%s"
+        end
+        local t={} ; i=1
+        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+                t[i] = str
+                i = i + 1
+        end
+        return t
 end
