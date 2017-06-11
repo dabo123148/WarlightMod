c4c59a8c4e2110b864d43db4dd3001242dce93a5
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-11 17:27:47 +0200

    update

--- SimpleDiploMod/Server_AdvanceTurn.lua
+++ SimpleDiploMod/Server_AdvanceTurn.lua
@@ -49,15 +49,15 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 							playerGameData[order.PlayerID].Money = Mod.PlayerGameData[order.PlayerID].Money+ Mod.Settings.MoneyPerCapturedTerritory;
 						end
 					end
-					local match = false;
-					for _,terrid in pairs(game.Map.Bonuses[game.Map.Territories[order.to].PartOfBonuses].Territories)do
-						if(game.ServerGame.Territories[terrid].OwnerPlayerID ~= order.PlayerID and terrid ~= order.To)then
-							match = true;
-						end
-					end
-					if(match == true)then
-						playerGameData[order.PlayerID].Money = Mod.PlayerGameData[order.PlayerID].Money+ Mod.Settings.MoneyPerCapturedBonus;
-					end
+					--local match = false;
+					--for _,terrid in pairs(game.Map.Bonuses[game.Map.Territories[order.To].PartOfBonuses].Territories)do
+					--	if(game.ServerGame.Territories[terrid].OwnerPlayerID ~= order.PlayerID and terrid ~= order.To)then
+					--		match = true;
+					--	end
+					--end
+					--if(match == true)then
+					--	playerGameData[order.PlayerID].Money = Mod.PlayerGameData[order.PlayerID].Money+ Mod.Settings.MoneyPerCapturedBonus;
+					--end
 				end
 				local toowner = game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID;
 				for _,spieler in pairs(AllPlayerIDs)do
