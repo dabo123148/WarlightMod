a82fc4a92133b7a12220f4d2b1d36ec02bd21aec
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-15 20:04:34 +0200

    update

--- Custom_Card_Package/Server_StartGame.lua
+++ Custom_Card_Package/Server_StartGame.lua
@@ -2,9 +2,16 @@ function Server_StartGame(game,standing)
   local playerGameData = Mod.PlayerGameData;
   for playerID in pairs(game.ServerGame.Game.PlayingPlayers) do
     if(playerID>50)then
-      Pieces = Mod.Settings.PestCardStartPieces%Mod.Settings.PestCardPiecesNeeded;
-      Cards = (Mod.Settings.PestCardStartPieces-Pieces)/Mod.Settings.PestCardPiecesNeeded;
-      playerGameData[playerID]={ PestCardPieces=Pieces, PestCards=Cards, SuccessfullyAttacked=0 };
+      PestPieces = Mod.Settings.PestCardStartPieces%Mod.Settings.PestCardPiecesNeeded;
+      PestCards = (Mod.Settings.PestCardStartPieces-PestPieces)/Mod.Settings.PestCardPiecesNeeded;
+      NukePieces = Mod.Settings.NukeCardStartPieces%Mod.Settings.NukeCardPiecesNeeded;
+      NukeCards = (Mod.Settings.NukeCardStartPieces-NukePieces)/Mod.Settings.NukeCardPiecesNeeded;
+      playerGameData[playerID]={};
+      playerGameData[playerID].PestCardPieces=PestPieces;
+      playerGameData[playerID].PestCards=PestCards;
+      playerGameData[playerID].NukeCardPieces=NukePieces;
+      playerGameData[playerID].NukeCards=NukeCards;
+      playerGameData[playerID].SuccessfullyAttacked=0;
     end
   end
   local publicGameData = Mod.PublicGameData;
