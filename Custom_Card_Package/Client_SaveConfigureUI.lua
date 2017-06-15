0e1bc331b422ad2c7e7b54fa68fb5afbbffd93de
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-15 18:29:19 +0200

    test

--- Custom_Card_Package/Client_SaveConfigureUI.lua
+++ Custom_Card_Package/Client_SaveConfigureUI.lua
@@ -1,6 +1,7 @@
 
 function Client_SaveConfigureUI(alert)
         Mod.Settings.PestCardIn=PestCardCheckbox.GetIsChecked();
+        print(PestCardIn);
         if(PestCardIn==true)then
                  Mod.Settings.PestCardStrength=PestCardStrengthSlider.GetValue();
                  Mod.Settings.PestCardPiecesNeeded=PestCardPiecesNeededBox.GetValue();
