5ac7236544216b99b68fab5b84d47c336678df3a
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-15 19:32:53 +0200

    bugfix

--- Custom_Card_Package/Client_PresentSettingsUI.lua
+++ Custom_Card_Package/Client_PresentSettingsUI.lua
@@ -16,23 +16,23 @@ function Client_PresentSettingsUI(rootParent)
 		horz = UI.CreateHorizontalLayoutGroup(rootParent);
 		UI.CreateLabel(horz).SetText('Nuke Card Settings:');
 		horz = UI.CreateHorizontalLayoutGroup(rootParent);
-		UI.CreateLabel(vert).SetText('Territories hit by a nuke take ' .. (Mod.Settings.NukeCardMainTerritoryDamage) .. '% Damage');
+		UI.CreateLabel(horz).SetText('Territories hit by a nuke take ' .. (Mod.Settings.NukeCardMainTerritoryDamage) .. '% Damage');
 		horz = UI.CreateHorizontalLayoutGroup(rootParent);
-		UI.CreateLabel(vert).SetText('Connected Territories to a nuke take ' .. (Mod.Settings.NukeCardConnectedTerritoryDamage) .. '% Damage');
+		UI.CreateLabel(horz).SetText('Connected Territories to a nuke take ' .. (Mod.Settings.NukeCardConnectedTerritoryDamage) .. '% Damage');
 		horz = UI.CreateHorizontalLayoutGroup(rootParent);
-		UI.CreateLabel(vert).SetText('Players can harm themselves: ' .. booltostring(Mod.Settings.Friendlyfire));
+		UI.CreateLabel(horz).SetText('Players can harm themselves: ' .. booltostring(Mod.Settings.Friendlyfire));
 		horz = UI.CreateHorizontalLayoutGroup(rootParent);
 		if(Mod.Settings.AfterDeployment)then
-			UI.CreateLabel(vert).SetText('Territories get nuked AFTER Deployment but before Gift and Blockade Cards take effect');
+			UI.CreateLabel(horz).SetText('Territories get nuked AFTER Deployment but before Gift and Blockade Cards take effect');
 		else
-			UI.CreateLabel(vert).SetText('Territories get nuked BEFORE Deployment');
+			UI.CreateLabel(horz).SetText('Territories get nuked BEFORE Deployment');
 		end
 		horz = UI.CreateHorizontalLayoutGroup(rootParent);
 		UI.CreateLabel(horz).SetText('Card Pieces Needed: ' .. Mod.Settings.NukeCardPiecesNeeded);
 		horz = UI.CreateHorizontalLayoutGroup(rootParent);
 		UI.CreateLabel(horz).SetText('Card Pieces Given in the beginning of the game: ' .. Mod.Settings.NukeCardStartPieces);
-		
 		horz = UI.CreateHorizontalLayoutGroup(rootParent);
+		UI.CreateLabel(horz).SetText(' ');
 	end
 end
 function booltostring(variable)
