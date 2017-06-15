6bf66294c0600ddaeaf59a1d2e2ed4b36beb0fc1
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-15 08:43:25 +0200

    update

--- SimpleDiploMod/Client_PresentSettingsUI.lua
+++ SimpleDiploMod/Client_PresentSettingsUI.lua
@@ -6,6 +6,7 @@ function Client_PresentSettingsUI(rootParent)
 	UI.CreateLabel(rootParent).SetText('Alliance Settings');
 	UI.CreateLabel(rootParent).SetText('Allied players can see your territories : ' .. booltostring(Mod.Settings.SeeAllyTerritories));
 	UI.CreateLabel(rootParent).SetText('Allies are visible to everyone : ' .. booltostring(Mod.Settings.PublicAllies));
+	if(Mod.Settings.StartMoney ~= 0 or Mod.Settings.MoneyPerTurn ~= 0 or Mod.Settings.MoneyPerKilledArmy ~= 0 or Mod.Settings.MoneyPerCapturedTerritory ~= 0 or Mod.Settings.MoneyPerCapturedBonus ~= 0)then
 	UI.CreateLabel(rootParent).SetText('Tradement System');
 	UI.CreateLabel(rootParent).SetText('Player starting money : ' .. Mod.Settings.StartMoney);
 	UI.CreateLabel(rootParent).SetText('Extra money per turn : ' .. Mod.Settings.MoneyPerTurn);
@@ -13,6 +14,10 @@ function Client_PresentSettingsUI(rootParent)
 	UI.CreateLabel(rootParent).SetText('Extra money per captured territory : ' .. Mod.Settings.MoneyPerCapturedTerritory);
 	UI.CreateLabel(rootParent).SetText('Extra money per captured bonus : ' .. Mod.Settings.MoneyPerCapturedBonus);
 	UI.CreateLabel(rootParent).SetText('Price per army : ' .. Mod.Settings.MoneyPerBoughtArmy);
+	else
+		UI.CreateLabel(rootParent).SetText('Tradement System');
+		UI.CreateLabel(rootParent).SetText('The Trading System has been disabled');
+	end
 	UI.CreateLabel(rootParent).SetText('Cards that require War');
 	UI.CreateLabel(rootParent).SetText('Sanction Cards can only be played on players you are in war with : ' .. booltostring(Mod.Settings.SanctionCardRequireWar));
 	UI.CreateLabel(rootParent).SetText('Bomb Cards can only be played on players you are in war with : ' .. booltostring(Mod.Settings.BombCardRequireWar));
