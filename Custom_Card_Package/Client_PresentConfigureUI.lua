638f7535d1255d5f14c9c775c76baf88afbfa828
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-15 17:50:48 +0200

    update

--- Custom_Card_Package/Client_PresentConfigureUI.lua
+++ Custom_Card_Package/Client_PresentConfigureUI.lua
@@ -21,50 +21,26 @@ function Client_PresentConfigureUI(rootParent)
 	local horzlist = {};
 	horzlist[0] = UI.CreateHorizontalLayoutGroup(rootParent);
 	PestCardCheckbox=UI.CreateCheckBox(horzlist[0]).SetText('Include Pestilence Card').SetIsChecked(PestCardIn).SetOnValueChanged(function() 
-			UI.CreateLabel(horzlist[1]).SetText('Pestilence Card Strength:');
-			PestCardStrengthSlider = UI.CreateNumberInputField(horzlist[1]).SetSliderMinValue(1).SetSliderMaxValue(3).SetValue(PestCardStrengthinit);
-			UI.CreateLabel(horzlist[2]).SetText('Card Pieces Needed:');
-			PestCardPiecesNeededBox=UI.CreateNumberInputField(horzlist[2]).SetSliderMinValue(1).SetSliderMaxValue(20).SetValue(PestCardPiecesNeeded);
-			UI.CreateLabel(horzlist[3]).SetText('Card Pieces given at the beginning of the game:');
-			PestCardStartPiecesBox=UI.CreateNumberInputField(horzlist[3]).SetSliderMinValue(0).SetSliderMaxValue(20).SetValue(PestCardStartPieces);
+			if(PestCardStrengthSlider ~= nil)then
+				UI.Destroy(text1);
+				UI.Destroy(PestCardStrengthSlider);
+				UI.Destroy(text2);
+				UI.Destroy(PestCardPiecesNeededBox);
+				UI.Destroy(text3);
+				UI.Destroy(PestCardStartPiecesBox);
+				PestCardStrengthSlider = nil;
+			else
+				text1 = UI.CreateLabel(horzlist[1]).SetText('Pestilence Card Strength:');
+				PestCardStrengthSlider = UI.CreateNumberInputField(horzlist[1]).SetSliderMinValue(1).SetSliderMaxValue(3).SetValue(PestCardStrengthinit);
+				text2 = UI.CreateLabel(horzlist[2]).SetText('Card Pieces Needed:');
+				PestCardPiecesNeededBox=UI.CreateNumberInputField(horzlist[2]).SetSliderMinValue(1).SetSliderMaxValue(20).SetValue(PestCardPiecesNeeded);
+				text3 = UI.CreateLabel(horzlist[3]).SetText('Card Pieces given at the beginning of the game:');
+				PestCardStartPiecesBox=UI.CreateNumberInputField(horzlist[3]).SetSliderMinValue(0).SetSliderMaxValue(20).SetValue(PestCardStartPieces);
+			end
 		end);
 	horzlist[1] = UI.CreateHorizontalLayoutGroup(rootParent);
 	horzlist[2] = UI.CreateHorizontalLayoutGroup(rootParent);
 	horzlist[3] = UI.CreateHorizontalLayoutGroup(rootParent);
 	horzlist[4] = UI.CreateHorizontalLayoutGroup(rootParent);
 	--PestCardCheckbox=UI.CreateCheckBox(horzlist[0]).SetText('Include Nuke Card').SetIsChecked(PestCardIn).SetOnValueChanged(PestCardCheckBoxChanged);
-end
-
-function PestCardCheckBoxChanged()
-	--PestCardStrength=1;
-   	-- Mod.Settings.PestCardStrength=1;
-   	-- PestCardStrengthSlider=nil;
-		
-   	-- PestCardPiecesNeeded=11;
-   	-- Mod.Settings.PestCardPiecesNeeded=11;
-   	-- PestCardPiecesNeededBox=nil;
-		
-   	-- PestCardStartPieces=0;
-    	--Mod.Settings.PestCardStartPieces=0;
-    	--PestCardStartPiecesBox=nil;
-	--PestCardIn= not PestCardIn;
-   	if(PestCardIn) then
-		lab1=UI.CreateLabel(vertPest).SetText('Pestilence Card Strength:');
-       		PestCardStrengthSlider=UI.CreateNumberInputField(vertPest).SetSliderMinValue(1).SetSliderMaxValue(3).SetValue(PestCardStrength);
-		
-		lab2=UI.CreateLabel(vertPest).SetText('Card Pieces Needed:');
-		PestCardPiecesNeededBox=UI.CreateNumberInputField(vertPest).SetSliderMinValue(1).SetSliderMaxValue(20).SetValue(PestCardPiecesNeeded);
-	
-		lab3=UI.CreateLabel(vertPest).SetText('Card Pieces given at the beginning of the game:');
-		PestCardStartPiecesBox=UI.CreateNumberInputField(vertPest).SetSliderMinValue(0).SetSliderMaxValue(20).SetValue(PestCardStartPieces);
-		Mod.Settings.PestCardIn=true;
-   	else
-		UI.Destroy(PestCardStrengthSlider);
-		UI.Destroy(PestCardPiecesNeededBox);
-		UI.Destroy(PestCardStartPiecesBox);
-		UI.Destroy(lab1);
-		UI.Destroy(lab2);
-		UI.Destroy(lab3);
-		Mod.Settings.PestCardIn=false;
-    	end
 end
