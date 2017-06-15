65268291c92abc786800d44ad8303faa7ecceb39
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-15 20:41:21 +0200

    bugfix

--- Custom_Card_Package/Server_AdvanceTurn.lua
+++ Custom_Card_Package/Server_AdvanceTurn.lua
@@ -69,7 +69,7 @@ function Server_AdvanceTurn_Order(game,gameOrder,result,skip,addOrder)
 					Effect[tablelength(Effect)+1] = WL.TerritoryModification.Create(targetterritoryid);
 					Effect[tablelength(Effect)].SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[conn.ID].NumArmies.NumArmies*(1-(Mod.Settings.NukeCardMainTerritoryDamage/100));
 				end
-				addOrder(WL.GameOrderEvent.Create(order.PlayerID,'Nuked ' .. game.Map.Territories[targetterritoryid].Name,{},Effect);
+				addOrder(WL.GameOrderEvent.Create(order.PlayerID,'Nuked ' .. game.Map.Territories[targetterritoryid].Name,{},Effect));
 			end
 			AusstehendeNukes = {};
 			addOrder(gameOrder);
