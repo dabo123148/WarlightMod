9fcf8dc30d86dd6a86fb6d621c4c671158e6ed7d
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-15 20:08:28 +0200

    update

--- Custom_Card_Package/Server_StartGame.lua
+++ Custom_Card_Package/Server_StartGame.lua
@@ -1,16 +1,20 @@
 function Server_StartGame(game,standing)
   local playerGameData = Mod.PlayerGameData;
   for playerID in pairs(game.ServerGame.Game.PlayingPlayers) do
-    if(playerID>50)then
-      PestPieces = Mod.Settings.PestCardStartPieces%Mod.Settings.PestCardPiecesNeeded;
-      PestCards = (Mod.Settings.PestCardStartPieces-PestPieces)/Mod.Settings.PestCardPiecesNeeded;
-      NukePieces = Mod.Settings.NukeCardStartPieces%Mod.Settings.NukeCardPiecesNeeded;
-      NukeCards = (Mod.Settings.NukeCardStartPieces-NukePieces)/Mod.Settings.NukeCardPiecesNeeded;
-      playerGameData[playerID]={};
-      playerGameData[playerID].PestCardPieces=PestPieces;
-      playerGameData[playerID].PestCards=PestCards;
-      playerGameData[playerID].NukeCardPieces=NukePieces;
-      playerGameData[playerID].NukeCards=NukeCards;
+      if(playerID>50)then
+        playerGameData[playerID]={};
+        if(Mod.Settings.PestCardIn)then
+          PestPieces = Mod.Settings.PestCardStartPieces%Mod.Settings.PestCardPiecesNeeded;
+          PestCards = (Mod.Settings.PestCardStartPieces-PestPieces)/Mod.Settings.PestCardPiecesNeeded;
+          playerGameData[playerID].PestCardPieces=PestPieces;
+          playerGameData[playerID].PestCards=PestCards;
+        end
+       if(Mod.Settings.NukeCardIn)then
+          NukePieces = Mod.Settings.NukeCardStartPieces%Mod.Settings.NukeCardPiecesNeeded;
+          NukeCards = (Mod.Settings.NukeCardStartPieces-NukePieces)/Mod.Settings.NukeCardPiecesNeeded;
+          playerGameData[playerID].NukeCardPieces=NukePieces;
+          playerGameData[playerID].NukeCards=NukeCards;
+       end
       playerGameData[playerID].SuccessfullyAttacked=0;
     end
   end
