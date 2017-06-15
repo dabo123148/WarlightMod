551d30ee84d5cf44250954deb7b3629e0b1e69f7
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-15 18:36:58 +0200

    update

--- Custom_Card_Package/Client_SaveConfigureUI.lua
+++ Custom_Card_Package/Client_SaveConfigureUI.lua
@@ -1,9 +1,18 @@
 
 function Client_SaveConfigureUI(alert)
         Mod.Settings.PestCardIn=PestCardCheckbox.GetIsChecked();
-        if( Mod.Settings.PestCardIn==true)then
+        if(Mod.Settings.PestCardIn==true)then
                  Mod.Settings.PestCardStrength=PestCardStrengthSlider.GetValue();
                  Mod.Settings.PestCardPiecesNeeded=PestCardPiecesNeededBox.GetValue();
                  Mod.Settings.PestCardStartPieces=PestCardStartPiecesBox.GetValue();
+        end
+        Mod.Settings.NukeCardIn =PestCardCheckbox.GetIsChecked();
+        if(Mod.Settings.NukeCardIn == true)then
+                Mod.Settings.AfterDeployment = AfterDeploymentToggle.GetValue;
+                Mod.Settings.Friendlyfire=FriendlyfireToggle.GetIsChecked();
+                Mod.Settings.NukeCardPiecesNeeded=NukeCardPiecesNeededBox.GetValue();
+                Mod.Settings.NukeCardStartPieces=NukeCardStartPiecesBox.GetValue();
+                Mod.Settings.NukeCardMainTerritoryDamage=NukeCardMainTerritoryDamageSlider.GetValue();
+                Mod.Settings.NukeCardConnectedTerritoryDamage=NukeCardConnectedTerritoryDamageSlider.GetValue();
         end
 end
