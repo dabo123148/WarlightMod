903ddfa9f064889e0e42b63457d61c60ef5575c8
dabo123148 <dabo123148@users.noreply.github.com>
2017-06-15 18:25:30 +0200

    bugfix

--- Custom_Card_Package/Client_SaveConfigureUI.lua
+++ Custom_Card_Package/Client_SaveConfigureUI.lua
@@ -1,8 +1,9 @@
 
 function Client_SaveConfigureUI(alert)
-    Mod.Settings.PestCardIn=PestCardCheckbox.GetIsChecked();
-    Mod.Settings.PestCardStrength=PestCardStrengthSlider.GetValue();
-    Mod.Settings.PestCardPiecesNeeded=PestCardPiecesNeededBox.GetValue();
-    Mod.Settings.PestCardStartPieces=PestCardStartPiecesBox.GetValue();
-    
+        Mod.Settings.PestCardIn=PestCardCheckbox.GetIsChecked();
+        if(PestCardIn==true)then
+                 Mod.Settings.PestCardStrength=PestCardStrengthSlider.GetValue();
+                 Mod.Settings.PestCardPiecesNeeded=PestCardPiecesNeededBox.GetValue();
+                 Mod.Settings.PestCardStartPieces=PestCardStartPiecesBox.GetValue();
+        end
 end
