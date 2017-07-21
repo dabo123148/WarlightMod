e3fee66c7e17ac74dc21dabf341f06e8a642e0a6
dabo123148 <dabo123148@users.noreply.github.com>
2017-07-21 16:21:47 +0200

    bugfix

--- SimpleDiploMod/Client_PresentSettingsUI.lua
+++ SimpleDiploMod/Client_PresentSettingsUI.lua
@@ -65,7 +65,7 @@ function CreateLine(settingname,variable,default,important)
 	else
 		lab.SetText(settingname .. variable);
 	end
-	if(varaible ~= default)then
+	if(variable ~= default)then
 		if(important == true)then
 			lab.SetColor('#FF0000');
 		else
