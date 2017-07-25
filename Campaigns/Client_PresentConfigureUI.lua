f931e19dae203cd4849af978e2b6766c6c17d892
RealMelwei <melvin@weissprivat.de>
2017-07-25 20:48:37 +0200

    Update Client_PresentConfigureUI.lua

--- Campaigns/Client_PresentConfigureUI.lua
+++ Campaigns/Client_PresentConfigureUI.lua
@@ -1,24 +1,27 @@
 
 function Client_PresentConfigureUI(rootParent)
+	PubRoot=rootParent;
 	local initialValue1 = Mod.Settings.Campaign;
 	if initialValue1 == nil then initialValue1 = {}; end
 	
 	lineCount=0;
-
-        vert1 = UI.CreateVerticalLayoutGroup(rootParent);
+	
+        vert1 = UI.CreateVerticalLayoutGroup(PubRoot);
 	UI.CreateLabel(vert1).SetText('Your Campaign. Enter Commands.');
 	button1 = UI.CreateButton(vert1);
 	button1.SetText('Add Line');
 	button1.SetOnClick(AddLine);
 	lines = {};
-	
+	fields = {};
 
 end
 
 function AddLine()
 	lineCount=lineCount+1;
 	UI.Destroy(button1);
-	lines[lineCount]=UI.CreateTextInputField(vert1).SetPreferredWidth(200).SetPreferredHeight(30).SetPlaceholderText('Add Command');
+	lines[lineCount]=UI.CreateHorizontalLayoutGroup(PubRoot);
+	local Numberindex=UI.CreateLabel(lines[lineCount]).SetText(lineCount);
+	fields[lineCount] = UI.CreateTextInputField(lines[lineCount]).SetPreferredWidth(500).SetPreferredHeight(30).SetPlaceholderText('Add Command');
 	button1 = UI.CreateButton(vert1);
 	button1.SetText('Add Line');
 	button1.SetOnClick(AddLine);
