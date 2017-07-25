2091b9f31a4811f56910df123aa6e10a70775ec8
RealMelwei <melvin@weissprivat.de>
2017-07-25 21:43:45 +0200

    Update Client_PresentConfigureUI.lua

--- Campaigns/Client_PresentConfigureUI.lua
+++ Campaigns/Client_PresentConfigureUI.lua
@@ -52,7 +52,6 @@ function DeleteLine()
 			UI.Destroy(lines[i-1]);
 			--end
 			lines[i-1]=UI.CreateHorizontalLayoutGroup(PubRoot);
-			adresses[i-1]=UI.CreateLabel(lines[i-1]).SetText('l'..(i-1)..':');
 			fields[i-1] = UI.CreateTextInputField(lines[i-1]).SetPreferredWidth(500).SetPreferredHeight(30).SetText(fields[i].GetText()).SetFlexibleHeight(1);
 	
 			if(i==lineCount+1)then
