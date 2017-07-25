44f76f4020960e0eabfa65952ad7cd80de1b5386
RealMelwei <melvin@weissprivat.de>
2017-07-25 20:38:16 +0200

    Update Client_PresentConfigureUI.lua

--- Campaigns/Client_PresentConfigureUI.lua
+++ Campaigns/Client_PresentConfigureUI.lua
@@ -18,8 +18,7 @@ end
 function AddLine()
 	lineCount=lineCount+1;
 	UI.Destroy(button1);
-	lines[lineCount]=UI.CreateTextInputField(vert1);
-	lines[lineCount].FlexibleWidth=1;
+	lines[lineCount]=UI.CreateTextInputField(vert1).SetPreferredWidth(200).SetPreferredHeight(30);
 	button1 = UI.CreateButton(vert1);
 	button1.SetText('Add Line');
 	button1.SetOnClick(AddLine);
