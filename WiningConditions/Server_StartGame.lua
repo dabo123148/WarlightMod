9feaa5351704974b038b0d75c1a464f592874af1
dabo123148 <dabo123148@users.noreply.github.com>
2017-07-25 10:52:15 +0200

    bugfix

--- WiningConditions/Server_StartGame.lua
+++ WiningConditions/Server_StartGame.lua
@@ -2,19 +2,19 @@ function Server_StartGame(game,standing)
    local playerGameData = Mod.PlayerGameData;
 	for _,pid in pairs(game.ServerGame.Game.PlayingPlayers)do
 		if(pid.IsAI == false)then
-			playerGameData[pid] = {};
-			playerGameData[pid].Capturedterritories = 0;
-			playerGameData[pid].Lostterritories = 0;
-			playerGameData[pid].Ownedterritories = 0;
-			playerGameData[pid].Capturedbonuses = 0;
-			playerGameData[pid].Lostbonuses = 0;
-			playerGameData[pid].Ownedbonuses = 0;
-			playerGameData[pid].Killedarmies = 0;
-			playerGameData[pid].Lostarmies = 0;
-			playerGameData[pid].Ownedarmies = 0;
-			playerGameData[pid].Eleminateais = 0;
-			playerGameData[pid].Eleminateplayers = 0;
-			playerGameData[pid].Eleminateaisandplayers = 0;
+			playerGameData[pid.ID] = {};
+			playerGameData[pid.ID].Capturedterritories = 0;
+			playerGameData[pid.ID].Lostterritories = 0;
+			playerGameData[pid.ID].Ownedterritories = 0;
+			playerGameData[pid.ID].Capturedbonuses = 0;
+			playerGameData[pid.ID].Lostbonuses = 0;
+			playerGameData[pid.ID].Ownedbonuses = 0;
+			playerGameData[pid.ID].Killedarmies = 0;
+			playerGameData[pid.ID].Lostarmies = 0;
+			playerGameData[pid.ID].Ownedarmies = 0;
+			playerGameData[pid.ID].Eleminateais = 0;
+			playerGameData[pid.ID].Eleminateplayers = 0;
+			playerGameData[pid.ID].Eleminateaisandplayers = 0;
 		end
 	end
    Mod.PlayerGameData = playerGameData;
