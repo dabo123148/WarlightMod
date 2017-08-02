5b5d167c68e18f7e535cbf92dbdc24b8f82f1922
dabo123148 <dabo123148@users.noreply.github.com>
2017-08-02 15:37:02 +0200

    bugfix

--- AdvancedDiploMod/Client_GameRefresh.lua
+++ AdvancedDiploMod/Client_GameRefresh.lua
@@ -1,20 +1,14 @@
 require('History');
 function Client_GameRefresh(game)
 	local Nachricht = "";
-  	if(Mod.PlayerGameData.Peaceoffers~=nil)then
-    		if(tablelength(Mod.PlayerGameData.Peaceoffers)>0)then
-    			Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.Peaceoffers) .. ' open peace requests';
-   		end
-  	end
-  	if(Mod.PlayerGameData.Allyoffers~=nil)then
-    		if(stringtotable(Mod.PlayerGameData.AllyOffers)>0)then
-      			Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.Allyoffers) .. ' open ally requests';
-    		end
- 	 end
-  	if(Mod.PlayerGameData.Terrselloffers~=nil)then
-    		if(stringtotable(Mod.PlayerGameData.Terrselloffers)>0)then
-      			Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.Terrselloffers) .. ' open territory tradement requests';
-    		end
-  	end
+    	if(tablelength(Mod.PlayerGameData.Peaceoffers)>0)then
+    		Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.Peaceoffers) .. ' open peace requests';
+   	end
+    	if(tablelength(Mod.PlayerGameData.AllyOffers)>0)then
+      		Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.Allyoffers) .. ' open ally requests';
+    	end
+    	if(tablelength(Mod.PlayerGameData.Terrselloffers)>0)then
+      		Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.Terrselloffers) .. ' open territory tradement requests';
+    	end
 	ShowHistory(Mod.PlayerGameData.NeueNachrichten,game,Nachricht);
 end
