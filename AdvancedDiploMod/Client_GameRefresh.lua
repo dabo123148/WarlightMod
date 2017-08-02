564733f8c522ef2d31188f4e04f7ed7810960ddb
dabo123148 <dabo123148@users.noreply.github.com>
2017-08-02 21:32:15 +0200

    update

--- AdvancedDiploMod/Client_GameRefresh.lua
+++ AdvancedDiploMod/Client_GameRefresh.lua
@@ -5,13 +5,13 @@ function Client_GameRefresh(game)
 	end
 	local Nachricht = "";
     	if(tablelength(Mod.PlayerGameData.Peaceoffers)>0)then
-    		Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.Peaceoffers) .. ' open peace requests';
+    		Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.Peaceoffers) .. ' open peace offer';
    	end
     	if(tablelength(Mod.PlayerGameData.AllyOffers)>0)then
-      		Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.AllyOffers) .. ' open ally requests';
+      		Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.AllyOffers) .. ' open ally offer';
     	end
     	if(tablelength(Mod.PlayerGameData.TerritorySellOffers)>0)then
-      		Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.TerritorySellOffers) .. ' open territory tradement requests';
+      		Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.TerritorySellOffers) .. ' open territory tradement offer';
     	end
 	ShowHistory(Mod.PlayerGameData.NeueNachrichten,game,Nachricht);
 end
