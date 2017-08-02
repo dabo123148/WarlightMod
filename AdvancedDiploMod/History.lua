63634c37845bc8a72ada5b0d0fdcd70206d6c4c0
dabo123148 <dabo123148@users.noreply.github.com>
2017-08-02 15:39:59 +0200

    removed

--- AdvancedDiploMod/History.lua
+++ AdvancedDiploMod/History.lua
@@ -108,32 +108,3 @@ function tablelength(T)
 	end
 	return count;
 end
-function stringtotable(variable)
-	chartable = {};
-	while(string.len(variable)>0)do
-		chartable[tablelength(chartable)] = string.sub(variable, 1 , 1);
-		variable = string.sub(variable, 2);
-	end
-	local newtable = {};
-	local tablepos = 0;
-	local executed = false;
-	for _, elem in pairs(chartable)do
-		if(elem == ",")then
-			tablepos = tablepos + 1;
-			newtable[tablepos] = "";
-			executed = true;
-		else
-			if(executed == false)then
-				tablepos = tablepos + 1;
-				newtable[tablepos] = "";
-				executed = true;
-			end
-			if(newtable[tablepos] == nil)then
-				newtable[tablepos] = elem;
-			else
-				newtable[tablepos] = newtable[tablepos] .. elem;
-			end
-		end
-	end
-	return newtable;
-end
