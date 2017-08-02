67c63ed73844954ac74c1fd471527d2634fda2c5
dabo123148 <dabo123148@users.noreply.github.com>
2017-08-02 16:18:02 +0200

    bugfix

--- AdvancedDiploMod/History.lua
+++ AdvancedDiploMod/History.lua
@@ -81,7 +81,7 @@ function ShowHistory(datentable,game,ende)
 			teildaten =  getname(data.Buyer,game) .. " tried to buy " .. getterrname(data.terrid,game) .. " from you, but he hadn't enough money";
 		end
 		if(teildaten ~= "")then
-			daten = daten .. "Turn " .. data.Turn .. ":".. teildaten .. "\n";
+			daten = daten .. "Turn " .. tostring(data.Turn+1) .. ":".. teildaten .. "\n";
 		end
 	end
 	if(daten ~= "")then
