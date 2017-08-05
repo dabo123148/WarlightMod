6336a409f9ff8596b02f41ae38411b8ad7d13af6
dabo123148 <dabo123148@users.noreply.github.com>
2017-08-05 08:23:29 +0200

    bugfix

--- AdvancedDiploMod/Client_GameRefresh.lua
+++ AdvancedDiploMod/Client_GameRefresh.lua
@@ -3,15 +3,17 @@ function Client_GameRefresh(game)
 	if(game.Us == nil)then
 		return;
 	end
-	local Nachricht = "";
-    	if(Mod.PlayerGameData.Peaceoffers ~= nil and tablelength(Mod.PlayerGameData.Peaceoffers)>0)then
-    		Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.Peaceoffers) .. ' open peace offer';
-   	end
-    	if(Mod.PlayerGameData.AllyOffers ~= nil and tablelength(Mod.PlayerGameData.AllyOffers)>0)then
-      		Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.AllyOffers) .. ' open ally offer';
-    	end
-    	if(Mod.PlayerGameData.TerritorySellOffers ~= nil and tablelength(Mod.PlayerGameData.TerritorySellOffers)>0)then
-      		Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.TerritorySellOffers) .. ' open territory tradement offer';
-    	end
-	ShowHistory(Mod.PlayerGameData.NeueNachrichten,game,Nachricht);
+	if(Mod.PlayerGameData.NeueNachrichten ~= nil)then
+		local Nachricht = "";
+    		if(tablelength(Mod.PlayerGameData.Peaceoffers)>0)then
+    			Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.Peaceoffers) .. ' open peace offer';
+   		end
+    		if(tablelength(Mod.PlayerGameData.AllyOffers)>0)then
+      			Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.AllyOffers) .. ' open ally offer';
+    		end
+    		if(tablelength(Mod.PlayerGameData.TerritorySellOffers)>0)then
+      			Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.TerritorySellOffers) .. ' open territory tradement offer';
+    		end
+		ShowHistory(Mod.PlayerGameData.NeueNachrichten,game,Nachricht);
+	end
 end
