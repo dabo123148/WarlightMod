a12553bcdbd069c3241bbafbb12f3ac9fdd2ea51
dabo123148 <dabo123148@users.noreply.github.com>
2017-08-17 10:32:19 +0200

    test

--- AdvancedDiploMod/Server_AdvanceTurn.lua
+++ AdvancedDiploMod/Server_AdvanceTurn.lua
@@ -302,8 +302,8 @@ function Server_AdvanceTurn_End (game,addNewOrder)
 			local moneyforplayer = {};
 			moneyforplayer[pid.ID] = {};
 			moneyforplayer[pid.ID][WL.ResourceType.Gold] = playerGameData[pid.ID].Money;
+			addNewOrder(WL.GameOrderEvent.Create(pid.ID, "Recieved " .. playerGameData[pid.ID].Money .. " Gold from Advanced Diplo Mod", {}, {},moneyforplayer));
 			playerGameData[pid.ID].Money = 0;
-			addNewOrder(WL.GameOrderEvent.Create(pid.ID, "Recieved Gold from Advanced Diplo Mod", {}, {},moneyforplayer));
 		end
 	end
 	
