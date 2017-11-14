718f5aa8125de1fa50235d9c80a4506ebb806a62
dabo123148 <dabo123148@users.noreply.github.com>
2017-11-14 15:27:17 +0100

    saving server resources

--- AdvancedDiploMod/Server_Created.lua
+++
@@ -1,4 +0,0 @@
-function Server_Created (game,settings)
-  --autoboot is not writable --> file not required(tested in singleplayer, maybe in multiplayer it works)
-  --settings.AutoBootEnabled = false;
-end
