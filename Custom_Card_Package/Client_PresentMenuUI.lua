bbccc851bb5af727390a60c606cbdec8c613bb31
RealMelwei <melvin@weissprivat.de>
2018-02-10 16:45:44 +0100

    Added Iso Card

--- Custom_Card_Package/Client_PresentMenuUI.lua
+++ Custom_Card_Package/Client_PresentMenuUI.lua
@@ -1,9 +1,9 @@
 function Client_PresentMenuUI(RootParent, setMaxSize, setScrollable, game,close)
 	if(game.Us==nil)then
-		UI.Alert('Spectators can not play cars');
+		UI.Alert('Spectators can not play cards');
 		return;
 	end
-	if(Mod.PlayerGameData.PestCards == nil and Mod.PlayerGameData.NukeCards == nil)then
+	if(Mod.PlayerGameData.PestCards == nil and Mod.PlayerGameData.NukeCards == nil and Mod.PlayerGameData.IsolationCards == nil)then
 		UI.Alert('You cannot play cards during distribution');
 		return;
 	end
@@ -21,6 +21,10 @@ function Client_PresentMenuUI(RootParent, setMaxSize, setScrollable, game,close)
    			NukeCards=0;
   		end
 	end
+	IsolationCards=Mod.PlayerGameData.IsolationCards;
+	if(IsolationCards==nil)then
+  	 	IsolationCards=0;
+  	end
 	ShowFirstMenu();
   	setMaxSize(450, 350);
 end
@@ -38,7 +42,7 @@ function ShowFirstMenu()
 			end
 	  	end
 		PestCardsFree=PestCards-PestCardsPlayed;
-    		vertPest=UI.CreateVerticalLayoutGroup(rootParent);
+    		Pest=UI.CreateVerticalLayoutGroup(rootParent);
     		PestText0=UI.CreateLabel(vertPest).SetText('Pestilence Card: ');
     		PestText1=UI.CreateLabel(vertPest).SetText('      You have got '..tostring(PestCardsFree)..' Cards and '..tostring(Mod.PlayerGameData.PestCardPieces)..'/'..Mod.Settings.PestCardPiecesNeeded..' Pieces.');
     		if(PestCardsFree>0)then
@@ -67,6 +71,25 @@ function ShowFirstMenu()
     			NukeButton1=UI.CreateButton(vertNuke).SetText('Play Nuke Card').SetOnClick(PlayNukeCard);
     	 	end
    	end
+	if(Mod.Settings.IsolationCardIn)then
+		IsolationCardsPlayed=0;
+		for _,order in pairs(Game.Orders) do
+			if(order.proxyType=="GameOrderCustom")then
+				if(order.Payload~=nil)then
+					if(split(order.Payload,'|')[1]=='Isolation')then
+						IsolationCardsPlayed=IsolationCardsPlayed+1;
+					end
+				end
+			end
+	  	end
+		IsolationCardsFree=IsolationCards-IsolationCardsPlayed;
+    		vertIsolation=UI.CreateVerticalLayoutGroup(rootParent);
+    		IsolationText0=UI.CreateLabel(vertIsolation).SetText('Isolation Card: ');
+    		IsolationText1=UI.CreateLabel(vertIsolation).SetText('      You have got '..tostring(IsolationCardsFree)..' Cards and '..tostring(Mod.PlayerGameData.IsolationCardPieces)..'/'..Mod.Settings.IsolationCardPiecesNeeded..' Pieces.');
+    		if(IsolationCardsFree>0)then
+      			IsolationButton1=UI.CreateButton(vertIsolation).SetText('Play Isolation Card').SetOnClick(PlayIsolationCard);
+    		end
+  	end
 end
 
 function PlayNukeCard()
@@ -76,15 +99,6 @@ function PlayNukeCard()
 	end
 	options = map(Game.Map.Territories,SelectTerritory);
 	UI.PromptFromList("Select the territory, you like to nuke", options);
-end
-function map(array, func)
-	local new_array = {};
-	local i = 1;
-	for _,v in pairs(array) do
-		new_array[i] = func(v);
-		i = i + 1;
-	end
-	return new_array;
 end
 function SelectTerritory(terr)
 	local name = terr.Name;
@@ -102,6 +116,33 @@ function SelectTerritory(terr)
 	end
 	return ret;
 end
+
+function PlayIsolationCard()
+	if(Game.Us.HasCommittedOrders == true)then
+		UI.Alert("You need to uncommit first");
+		return;
+	end
+	options = map(Game.Map.Territories,SelectTerritoryIso);
+	UI.PromptFromList("Select the territory to be isolated.", options);
+end
+function SelectTerritoryIso(terr)
+	local name = terr.Name;
+	local ret = {};
+	ret["text"] = name;
+	ret["selected"] = function() 
+		local orders = Game.Orders;
+		table.insert(orders, WL.GameOrderCustom.Create(Game.Us.ID, "Play an Isolation Card on " .. name, 'Isolation|'..terr.ID));
+		IsolationCardsFree=IsolationCardsFree-1;
+		if(IsolationCardsFree == 0)then
+			UI.Destroy(IsolationButton1);
+		end
+		NukeText1.SetText('      You have got '..tostring(IsolationCardsFree)..' Cards and '..tostring(Mod.PlayerGameData.IsolationCardPieces)..'/'..Mod.Settings.IsolationCardPiecesNeeded..' Pieces.');
+		Game.Orders = orders;
+	end
+	return ret;
+end
+
+
 function PlayPestCard()
 	if(Game.Us.HasCommittedOrders == true)then
 		UI.Alert("You need to uncommit first");
@@ -196,4 +237,13 @@ function split(inputstr, sep)
                 i = i + 1
         end
         return t
+end
+function map(array, func)
+	local new_array = {};
+	local i = 1;
+	for _,v in pairs(array) do
+		new_array[i] = func(v);
+		i = i + 1;
+	end
+	return new_array;
 end
