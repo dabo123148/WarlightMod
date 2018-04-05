cc46b566b9e6f222d96083ecf6e0bea8a64a4bb5
dabo123148 <dabo123148@users.noreply.github.com>
2018-04-05 20:00:08 +0200

    bugfix
    You no longer recieve a bug report, when a neutral territory gets bombed

--- WiningConditions/Server_AdvanceTurn.lua
+++ WiningConditions/Server_AdvanceTurn.lua
@@ -19,7 +19,7 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 			checkwin(order.PlayerID,addNewOrder,game);
 		end
 		local player2 = game.ServerGame.LatestTurnStanding.Territories[targetterr].OwnerPlayerID;
-		if(game.ServerGame.Game.Players[player2].IsAI == false)then
+		if(player2 ~= WL.PlayerID.Neutral and game.ServerGame.Game.Players[player2].IsAI == false)then
 			playerGameData[player2].Lostarmies = playerGameData[player2].Lostarmies+game.ServerGame.LatestTurnStanding.Territories[targetterr].NumArmies.NumArmies/2;
 			playerGameData[player2].Ownedarmies = playerGameData[player2].Ownedarmies - game.ServerGame.LatestTurnStanding.Territories[targetterr].NumArmies.NumArmies/2;;
 			checkwin(order.PlayerID,addNewOrder,game);
