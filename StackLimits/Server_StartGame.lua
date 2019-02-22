46df6f203667672d0f7482f050f9f91bc367420a
dabo123148 <dabo123148@users.noreply.github.com>
2019-02-22 21:39:01 +0100

    Update Server_StartGame.lua

--- StackLimits/Server_StartGame.lua
+++ StackLimits/Server_StartGame.lua
@@ -1,5 +1,3 @@
-require('Utility')
-
 function Server_StartGame(game, standing)
 	local Stacklimit = Mod.Settings.StackLimit;
 	local EffectNeutral = Mod.Settings.IncludeNeutral;
@@ -9,7 +7,7 @@ function Server_StartGame(game, standing)
 			if(territory.OwnerPlayerID ~= WL.PlayerID.Neutral or (territory.OwnerPlayerID == WL.PlayerID.Neutral and EffectNeutral == false))then
 				territory.NumArmies = WL.Armies.Create(Stacklimit,territory.NumArmies.SpecialUnits);
 			end
-        end
+       		end
   	end
 end
 
