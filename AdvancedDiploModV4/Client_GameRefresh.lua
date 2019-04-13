19b994b1f47e6e27ca2dd9126305affe2edf7a5b
dabo123148 <dabo123148@users.noreply.github.com>
2019-04-13 11:48:53 +0200

    potential fix for message overlapping

--- AdvancedDiploModV4/Client_GameRefresh.lua
+++ AdvancedDiploModV4/Client_GameRefresh.lua
@@ -1,13 +1,17 @@
 require('History');
 function Client_GameRefresh(game)
-	local Nachricht = "";
-    	if(tablelength(Mod.PlayerGameData.Peaceoffers)>0)then
-    		Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.Peaceoffers) .. ' open peace offer';
-   	end
-   	if(tablelength(Mod.PlayerGameData.AllyOffers)>0)then
-     		Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.AllyOffers) .. ' open ally offer';
-  	  end
-	ShowAllHistory(game,Nachricht);
+	if(showedreturnmessage == nil or showedreturnmessage == true)then
+		local Nachricht = "";
+    		if(tablelength(Mod.PlayerGameData.Peaceoffers)>0)then
+    			Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.Peaceoffers) .. ' open peace offer';
+   		end
+   		if(tablelength(Mod.PlayerGameData.AllyOffers)>0)then
+     			Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.AllyOffers) .. ' open ally offer';
+  		  end
+		ShowAllHistory(game,Nachricht);
+	else
+		showedreturnmessage = true;
+	end
 end
 function tablelength(T)
 	local count = 0;
