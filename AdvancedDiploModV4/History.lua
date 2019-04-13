58c4681578e6c5ca59243fe6f50b7db1b00e5636
dabo123148 <dabo123148@users.noreply.github.com>
2019-04-13 11:35:49 +0200

    Update History.lua

--- AdvancedDiploModV4/History.lua
+++ AdvancedDiploModV4/History.lua
@@ -22,6 +22,35 @@ function ShowHistory(datentable,game,ende)
 		end
 	end
 end
+function ShowAllHistory(game,ende)
+	local daten = "This history gets written into the history of the next turn:\n";
+	datentable = Mod.PublicGameData.History;
+	for _,data in pairs(datentable)do
+		daten = daten .. getname(data.By,game) .. ":".. data.Text .. "\n";
+	end
+	datentable = Mod.PlayerGameData.PrivateHistory;
+	for _,data in pairs(datentable)do
+		daten = daten .. getname(data.By,game) .. ":".. data.Text .. "\n";
+	end
+	if(ende == "" or ende == "showmessage")then
+		if(daten ~= "This history gets written into the history of the next turn:\n")then
+			--if(lastmessage == nil or lastmessage ~= daten or datentable == Mod.PlayerGameData.Nachrichten)then
+				--lastmessage = daten;
+				UI.Alert(daten);
+			--end
+		else
+			UI.Alert("There is currently no history");
+		end
+	else
+		daten = daten .. ende;
+		if(daten ~= "This history gets written into the history of the next turn:\n")then
+			--if(lastmessage == nil or lastmessage ~= daten or datentable == Mod.PlayerGameData.Nachrichten)then
+				--lastmessage = daten;
+				UI.Alert(daten);
+			--end
+		end
+	end
+end
 function getname(playerid,game)
 	print(playerid);
 	return game.Game.Players[playerid].DisplayName(nil, false);
