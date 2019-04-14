816a3aa13deec3386d1cac2d7fc44ce8171b6a31
dabo123148 <dabo123148@users.noreply.github.com>
2019-04-14 20:06:09 +0200

    Add files via upload

--- Bugreport/Server_GameCustomMessage.lua
+++ Bugreport/Server_GameCustomMessage.lua
@@ -3,6 +3,6 @@ function Server_GameCustomMessage(game, playerID, payload, setReturnTable)
 	publicGameData.Testvar = publicGameData.Testvar+1;
 	Mod.PublicGameData = publicGameData;
 	local rg = {};
-	rg.Message = "Resonse of custom message.";
+	rg.Message = "Response of custom message.";
 	setReturnTable(rg);
 end
