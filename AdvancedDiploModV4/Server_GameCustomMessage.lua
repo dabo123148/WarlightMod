3eef060233e60496b6127d73063f2b6a9261bc09
dabo123148 <dabo123148@users.noreply.github.com>
2019-05-08 21:15:12 +0200

    test

--- AdvancedDiploModV4/Server_GameCustomMessage.lua
+++ AdvancedDiploModV4/Server_GameCustomMessage.lua
@@ -9,6 +9,10 @@ function Server_GameCustomMessage(game, playerID, payload, setReturnTable)
 		publicGameData.test = publicGameData.test +1;
 		Mod.PublicGameData = publicGameData;
 		rg.Message = "Test";
+		local message = {};
+		message.By = playerID;
+		message.Text = " test history ";
+		publicGameData.History[tablelength(publicGameData.History)] = message;
 		setReturnTable(rg);
 	end
 	if(payload.Message == "Accept Allianze" or payload.Message == "Deny Allianze")then
