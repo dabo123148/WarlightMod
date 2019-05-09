f92b1b94a806cfe744919c51bd8d18de1971bb4c
dabo123148 <dabo123148@users.noreply.github.com>
2019-05-09 13:54:48 +0200

    Update Client_GameRefresh.lua

--- AdvancedDiploModV4/Client_GameRefresh.lua
+++ AdvancedDiploModV4/Client_GameRefresh.lua
@@ -6,18 +6,14 @@ function Client_GameRefresh(game)
 	if(lastnachricht == nil)then
 		lastnachricht = "";
 	end
-	if(showedreturnmessage == nil or showedreturnmessage == true)then
-		local Nachricht = "";
-    		if(tablelength(Mod.PlayerGameData.Peaceoffers)>0)then
-    			Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.Peaceoffers) .. ' open peace offer';
-   		end
-   		if(tablelength(Mod.PlayerGameData.AllyOffers)>0)then
-     			Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.AllyOffers) .. ' open ally offer';
-  		end
-		ShowAllHistory(game,Nachricht);
-	else
-		showedreturnmessage = true;
-	end
+	local Nachricht = "";
+    	if(tablelength(Mod.PlayerGameData.Peaceoffers)>0)then
+    		Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.Peaceoffers) .. ' open peace offer';
+   	end
+   	if(tablelength(Mod.PlayerGameData.AllyOffers)>0)then
+     		Nachricht = Nachricht .. "\n" .. 'You have ' .. tablelength(Mod.PlayerGameData.AllyOffers) .. ' open ally offer';
+  	end
+	ShowAllHistory(game,Nachricht);
 end
 function tablelength(T)
 	if(T==nil)then
