def256f2513c296531ef152fe5b1b24e36eece04
dabo123148 <dabo123148@users.noreply.github.com>
2019-05-09 20:35:49 +0200

    Update Client_PresentConfigureUI.lua

--- Dontloseaterritory/Client_PresentConfigureUI.lua
+++ Dontloseaterritory/Client_PresentConfigureUI.lua
@@ -2,11 +2,11 @@
 function Client_PresentConfigureUI(rootParent)
 	local initialValue = Mod.Settings.NoTerritory;
 	if initialValue == nil then
-		initialValue = true;
+		initialValue = false;
 	end
     
     local horz = UI.CreateHorizontalLayoutGroup(rootParent);
   	  InputNoTerritory = UI.CreateCheckBox(horz)
-		.SetText('If this is disabled, you lose the game once you lose any of your starting territories. If enabled, you lose the game if ANY of yours territories are taken.')
+		.SetText('Only effect starting territory.')
 		.SetIsChecked(initialValue);
 end
