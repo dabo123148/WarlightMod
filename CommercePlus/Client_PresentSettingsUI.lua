3cba9c47557f2b3868b6d633571093116f77549b
dabo123148 <dabo123148@users.noreply.github.com>
2019-05-09 21:03:46 +0200

    Update Client_PresentSettingsUI.lua

--- CommercePlus/Client_PresentSettingsUI.lua
+++ CommercePlus/Client_PresentSettingsUI.lua
@@ -3,13 +3,7 @@ function Client_PresentSettingsUI(rootParent)
 	root = rootParent;
 	CreateLine('Extra money per killed army : ', Mod.Settings.MoneyPerKilledArmy,1);
 	CreateLine('Extra money per captured territory : ', Mod.Settings.MoneyPerCapturedTerritory,5);
-	if(Mod.Settings.MoneyPerCapturedBonus == 0)then
-		local lab = UI.CreateLabel(root);
-		lab.SetText('Extra money per captured bonus : disabled');
-		lab.SetColor('#FFFF00');
-	else
-		CreateLine('Extra money per captured bonus : ', Mod.Settings.MoneyPerCapturedBonus,10);
-	end
+	CreateLine('Extra money per captured bonus : ', Mod.Settings.MoneyPerCapturedBonus,10);
 end
 function CreateLine(settingname,variable,default,important)
 	local lab = UI.CreateLabel(root);
@@ -17,7 +11,11 @@ function CreateLine(settingname,variable,default,important)
 	--This is just required in case new settings get added
 		lab.SetText(settingname .. default);
 	else
-		lab.SetText(settingname .. variable);
+		if(variable == 0)then
+			lab.SetText(settingname .. disabled);
+		else
+			lab.SetText(settingname .. variable);
+		end
 	end
 	if(variable ~= nil and variable ~= default)then
 	--when the settings isn't unknown(later added) and not default, this sets the text color to yellow
