e51afb9e03d25f8607b1a9c01d9fae015a0d75ce
dabo123148 <dabo123148@users.noreply.github.com>
2019-07-03 16:12:40 +0200

    Update Server_AdvanceTurn.lua

--- TransportOnlyAirlift/Server_AdvanceTurn.lua
+++ TransportOnlyAirlift/Server_AdvanceTurn.lua
@@ -1,11 +1,13 @@
 function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)	
 	if(order.proxyType == 'GameOrderPlayCardAirlift')then
 		local targetterritory = order.ToTerritoryID;
-		for _,terrid in pairs(game.ServerGame.LatestTurnStanding.Territories) do
-			if(terrid.OwnerPlayerID ~= order.PlayerID and terrid.OwnerPlayerID ~= WL.PlayerID.Neutral)then
+		for _,terrid in pairs(game.Map.Territories[targetterritory].ConnectedTo) do
+			local connowner = game.ServerGame.LatestTurnStanding.Territories[terrid.ID].OwnerPlayerID;
+			if(connowner ~= order.PlayerID and connowner ~= WL.PlayerID.Neutral)then
 				addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, "Skipped an airlift since the target territory was next to an enemy territory", {}, nil, nil));
 				skipThisOrder(WL.ModOrderControl.Skip);
 				return;
 			end
 		end
+	end
 end
