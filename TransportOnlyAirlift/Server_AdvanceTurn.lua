ea99cf9533be240c2974ffcce7eb1520e0bd5c27
dabo123148 <dabo123148@users.noreply.github.com>
2019-07-03 16:17:23 +0200

    Update Server_AdvanceTurn.lua

--- TransportOnlyAirlift/Server_AdvanceTurn.lua
+++ TransportOnlyAirlift/Server_AdvanceTurn.lua
@@ -3,10 +3,19 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 		local targetterritory = order.ToTerritoryID;
 		for _,terrid in pairs(game.Map.Territories[targetterritory].ConnectedTo) do
 			local connowner = game.ServerGame.LatestTurnStanding.Territories[terrid.ID].OwnerPlayerID;
-			if(connowner ~= order.PlayerID and connowner ~= WL.PlayerID.Neutral)then
-				addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, "Skipped an airlift since the target territory was next to an enemy territory", {}, nil, nil));
-				skipThisOrder(WL.ModOrderControl.Skip);
-				return;
+			local inteam = game.ServerGame.Game.Players[order.PlayerID].Team;
+			if(inteam ~= -1)then
+				if(connowner ~= order.PlayerID and connowner ~= WL.PlayerID.Neutral and game.ServerGame.Game.Players[connowner].Team ~= inteam)then
+					addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, "Skipped an airlift since the target territory was next to an enemy territory", {}, nil, nil));
+					skipThisOrder(WL.ModOrderControl.Skip);
+					return;
+				end
+			else
+				if(connowner ~= order.PlayerID and connowner ~= WL.PlayerID.Neutral)then
+					addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, "Skipped an airlift since the target territory was next to an enemy territory", {}, nil, nil));
+					skipThisOrder(WL.ModOrderControl.Skip);
+					return;
+				end
 			end
 		end
 	end
