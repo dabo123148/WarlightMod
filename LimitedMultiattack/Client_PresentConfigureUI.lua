bb9891f6e04fd1eb88403225a4e1130585a62e1c
dabo123148 <dabo123148@users.noreply.github.com>
2019-07-03 14:08:12 +0200

    Update Client_PresentConfigureUI.lua

--- LimitedMultiattack/Client_PresentConfigureUI.lua
+++ LimitedMultiattack/Client_PresentConfigureUI.lua
@@ -9,6 +9,12 @@ function Client_PresentConfigureUI(rootParent)
 	UI.CreateLabel(horz).SetText('Attacks per Multiattack(0=Infinit) ');
    	InputMaxAttacks = UI.CreateNumberInputField(horz).SetSliderMinValue(0).SetSliderMaxValue(100).SetValue(initialValue);
 	horz = UI.CreateHorizontalLayoutGroup(rootParent);
+	initialValue = Mod.Settings.ContinueAttackIfFailed;
+	if initialValue == nil then
+		initialValue = true;
+	end
+	InputContinueAttackIfFailed = UI.CreateCheckBox(horz).SetText('If an attack failed you can continue attacking from the field it failed from(standart multiattack is with this enabled)').SetIsChecked(initialValue);
+	horz = UI.CreateHorizontalLayoutGroup(rootParent);
 	UI.CreateLabel(horz).SetText('If one of the following checkboxes is activated, then you can only use multiattacks in turns, in which you play one of the following cards. The cards will still have their old effect.');
 	horz = UI.CreateHorizontalLayoutGroup(rootParent);
 	initialValue = Mod.Settings.ReinforcementCard;
