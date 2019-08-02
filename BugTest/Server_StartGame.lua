60c6137fcb768676faa764f2e13f0fa0509f0048
dabo123148 <dabo123148@users.noreply.github.com>
2019-08-03 01:01:20 +0200

    Update Server_StartGame.lua

--- BugTest/Server_StartGame.lua
+++ BugTest/Server_StartGame.lua
@@ -2,10 +2,6 @@ function Server_StartGame(game,standing)
 	local publicGameData = Mod.PublicGameData;
 	--Contains every War
 	publicGameData.War = {};
-	for _,pid in pairs(game.ServerGame.Game.Players) do
-		--Sets, that every player has no war going on
-		publicGameData.War[pid.ID] = "Test " .. pid.ID;
-	end
-	--Saves data
+	publicGameData.War[517210] = "517210";
 	Mod.PublicGameData = publicGameData;
 end
