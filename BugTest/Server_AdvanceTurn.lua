07a85e9eb4c79a45338e10d721140af774bd91aa
dabo123148 <dabo123148@users.noreply.github.com>
2019-08-03 01:00:13 +0200

    Update Server_AdvanceTurn.lua

--- BugTest/Server_AdvanceTurn.lua
+++ BugTest/Server_AdvanceTurn.lua
@@ -2,6 +2,6 @@ function Server_AdvanceTurn_Start (game,addNewOrder)
 	local publicGameData = Mod.PublicGameData;
 	--Contains every War
 	publicGameData.War = {};
-	publicGameData.War[517210] = "Test ";
+	publicGameData.War[517210] = "517210";
 	Mod.PublicGameData = publicGameData;
 end
