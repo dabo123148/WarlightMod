4ac3471067f22e53377a4d001cd8f04e530e62f4
dabo123148 <dabo123148@users.noreply.github.com>
2019-08-03 00:59:16 +0200

    Update Server_AdvanceTurn.lua

--- BugTest/Server_AdvanceTurn.lua
+++ BugTest/Server_AdvanceTurn.lua
@@ -2,10 +2,6 @@ function Server_AdvanceTurn_Start (game,addNewOrder)
 	local publicGameData = Mod.PublicGameData;
 	--Contains every War
 	publicGameData.War = {};
-	for _,pid in pairs(game.ServerGame.Game.Players) do
-		--Sets, that every player has no war going on
-		publicGameData.War[517210] = "Test " .. pid.ID;
-	end
-	--Saves data
+	publicGameData.War[517210] = "Test ";
 	Mod.PublicGameData = publicGameData;
 end
