baad5756d994ae4af8b8c27ff557f884c2f89af3
dabo123148 <dabo123148@users.noreply.github.com>
2019-08-02 22:11:45 +0200

    Update Client_PresentMenuUI.lua

--- AdvancedDiploModV4/Client_PresentMenuUI.lua
+++ AdvancedDiploModV4/Client_PresentMenuUI.lua
@@ -107,11 +107,12 @@ function OpenhistoryMenu()
 end
 function OpenMenu()
 	print("Testlenght " .. tablelength(Mod.PublicGameData.War));
-	for _,pd in pairs(Game.Game.PlayingPlayers)do
-		print("Testid" .. pd.ID);
-		if(Mod.PublicGameData.War[pd.ID] == {} or Mod.PublicGameData.War[pd.ID] ~= nil)then
-				print("Test" .. pd.ID);
-		end
+	for _,pd in pairs(Mod.PublicGameData.War)do
+				print(pd);
+		--print("Testid" .. pd.ID);
+		--if(Mod.PublicGameData.War[pd.ID] == {} or Mod.PublicGameData.War[pd.ID] ~= nil)then
+		--		print("Test" .. pd.ID);
+		--end
 	end
 	DeleteUI();
 	declarewarbutton = UI.CreateButton(vert).SetText("Declare War").SetOnClick(OpenDeclarWar);
