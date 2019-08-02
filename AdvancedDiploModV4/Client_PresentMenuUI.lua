62c01002fee9535098889c46b627a4f7579d9dee
dabo123148 <dabo123148@users.noreply.github.com>
2019-08-02 22:10:23 +0200

    Update Client_PresentMenuUI.lua

--- AdvancedDiploModV4/Client_PresentMenuUI.lua
+++ AdvancedDiploModV4/Client_PresentMenuUI.lua
@@ -109,7 +109,7 @@ function OpenMenu()
 	print("Testlenght " .. tablelength(Mod.PublicGameData.War));
 	for _,pd in pairs(Game.Game.PlayingPlayers)do
 		print("Testid" .. pd.ID);
-		if(Mod.PublicGameData.War[pd.ID] ~= nil)then
+		if(Mod.PublicGameData.War[pd.ID] == {} or Mod.PublicGameData.War[pd.ID] ~= nil)then
 				print("Test" .. pd.ID);
 		end
 	end
