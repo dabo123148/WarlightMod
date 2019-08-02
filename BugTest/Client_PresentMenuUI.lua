ace1d9e52edb989ba7c7f0b3dff5ef43b473f91f
dabo123148 <dabo123148@users.noreply.github.com>
2019-08-03 00:45:04 +0200

    Update Client_PresentMenuUI.lua

--- BugTest/Client_PresentMenuUI.lua
+++ BugTest/Client_PresentMenuUI.lua
@@ -6,30 +6,9 @@ function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game)
 		return;
 	end
 	--printing in order to visualize the bug
-	print("Testlenght " .. tablelength(Mod.PublicGameData.War));
 	for key,pd in pairs(Mod.PublicGameData.War)do
 		print(key .. " " .. pd);
 		vert = UI.CreateVerticalLayoutGroup(rootParent);
-		UI.CreateLabel(vert).SetText(key .. " " .. pd);
+		UI.CreateLabel(vert).SetText("Key " .. key .. " Real Key: " .. pd);
 	end
-	for _,pd in pairs(game.Game.PlayingPlayers)do
-		print("Playerid in  Client_PresentMenuUI.lua: " .. pd.ID);
-		if(Mod.PublicGameData.War[pd.ID] ~= nil)then
-			vert = UI.CreateVerticalLayoutGroup(rootParent);
-			UI.CreateLabel(vert).SetText("If this is printed then there is no bug existend, otherwise the mod can not see the data that is stored with the same playerid");
-			print("If this is printed then there is no bug existend, otherwise it can not see the data that is stored with the same playerid");
-		end
-	end
-	vert2 = UI.CreateVerticalLayoutGroup(rootParent);
-	UI.CreateLabel(vert2).SetText("If you only see this message, then the data is using a different playerid");
-end
-function  tablelength(T)
-	local count = 0;
-	if(T==nil)then
-		return 0;
-	end
-	for _, elem in pairs(T)do
-		count = count + 1;
-	end
-	return count;
 end
