4429e2dfa89925a83ddb46fda2535b269ef2e8e3
dabo123148 <dabo123148@users.noreply.github.com>
2020-10-12 19:24:46 +0200

    Delete Server_GameCustomMessage.lua

--- Bugreport/Server_GameCustomMessage.lua
+++
@@ -1,8 +0,0 @@
-function Server_GameCustomMessage(game, playerID, payload, setReturnTable)
-	publicGameData = Mod.PublicGameData;
-	publicGameData.Testvar = publicGameData.Testvar+1;
-	Mod.PublicGameData = publicGameData;
-	local rg = {};
-	rg.Message = "Response of custom message.";
-	setReturnTable(rg);
-end
