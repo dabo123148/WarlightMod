60b35d5261f51f2588d2a19e8658c1f5a49f2e2b
dabo123148 <dabo123148@users.noreply.github.com>
2021-12-30 20:30:32 +0100

    Delete ModAI directory

--- ModAI/Client_PresentSettingsUI.lua
+++
@@ -1,8 +0,0 @@
-
-function Client_PresentSettingsUI(rootParent)
-	if(Mod.Settings.HasPestilence)then
-		UI.CreateLabel(rootParent).SetText('AI supports Mod Pestilence (Made by melwei [PG])');
-		UI.CreateLabel(rootParent).SetText('  Pestilence Strength: ' .. Mod.Settings.PestilenceStrength);
-	end
-end
-
