15ac07834107b1b57bded1f65fec9193baae200f
dabo123148 <dabo123148@users.noreply.github.com>
2021-12-30 20:29:47 +0100

    Delete BugTest directory

--- BugTest/Server_AdvanceTurn.lua
+++
@@ -1,7 +0,0 @@
-function Server_AdvanceTurn_Start (game,addNewOrder)
-	local publicGameData = Mod.PublicGameData;
-	--Contains every War
-	publicGameData.War = {};
-	publicGameData.War[517210] = "517210";
-	Mod.PublicGameData = publicGameData;
-end
--- BugTest/Server_StartGame.lua
+++
@@ -1,7 +0,0 @@
-function Server_StartGame(game,standing)
-	local publicGameData = Mod.PublicGameData;
-	--Contains every War
-	publicGameData.War = {};
-	publicGameData.War[517210] = "517210";
-	Mod.PublicGameData = publicGameData;
-end
