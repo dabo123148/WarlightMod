45f617b6c2af6e0988299685023416b23c7c7cdc
dabo123148 <dabo123148@users.noreply.github.com>
2021-12-31 05:51:25 +0100

    Delete Runtime Wastelands directory

--- Runtime Wastelands/Client_PresentSettingsUI.lua
+++
@@ -1,7 +0,0 @@
-
-function Client_PresentSettingsUI(rootParent)
-	local vert = UI.CreateVerticalLayoutGroup(rootParent);
-	
-	UI.CreateLabel(vert).SetText('Wastelands per Turn:' .. Mod.Settings.WastelandsPerTurn);
-end
-
--- Runtime Wastelands/Client_SaveConfigureUI.lua
+++
@@ -1,6 +0,0 @@
-
-function Client_SaveConfigureUI(alert)
-    
-    Mod.Settings.WastelandsPerTurn = numberInputField1.GetValue();
-
-end
