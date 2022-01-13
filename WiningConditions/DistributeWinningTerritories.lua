69974521cd4202acc2f7377b562934222b644286
dabo123148 <dabo123148@users.noreply.github.com>
2022-01-13 05:14:25 +0100

    added indication for territories winning condition

---
+++ WiningConditions/DistributeWinningTerritories.lua
@@ -0,0 +1,25 @@
+function DistributeItems(game, standing)
+
+    for _,condition in pairs(Mod.Settings.terrcondition)do
+			local terrid = getterrid(game,condition.Terrname);
+      if(terrid ~= -1) then
+        local s = terrs[i].Structures;
+        if (s == nil) then 
+          s = {}; 
+        end;
+        if(s[WL.StructureType.Arena] == nil) then
+          s[WL.StructureType.Arena] = 1;
+        end
+        standing.Territories[terrid].Structures = s;
+      end
+    end
+
+end
+function getterrid(game,name)
+	for _,terr in pairs(game.Map.Territories)do
+		if(terr.Name == name)then
+			return terr.ID;
+		end
+	end
+	return -1;
+end
