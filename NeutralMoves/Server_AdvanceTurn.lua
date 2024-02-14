d9d546c4f09dcba0ac53b01edddc03e8783e214c
dabo123148 <dabo123148@users.noreply.github.com>
2024-02-14 17:13:05 +0100

    Update Server_AdvanceTurn.lua

--- NeutralMoves/Server_AdvanceTurn.lua
+++ NeutralMoves/Server_AdvanceTurn.lua
@@ -1,45 +1,94 @@
-function Server_AdvanceTurn_Start (game,addNewOrder)
-	executed = false;
-	if(tablelength(game.ServerGame.LatestTurnStanding.Territories) > 500)then
-		executed = true;
+local payload = 'NeutralMoves_ServerAdvanceTurnEnd';
+local executed = false;
+
+function Server_AdvanceTurn_Start(game, addNewOrder)
+	if Mod.PublicGameData.cantRun ~= nil then
+		return;
 	end
-	terrmodnum = 1;
-	terrMod = {}
-end
-function Server_AdvanceTurn_End(game,addNewOrder)
-	local currentarmies = {};
-	if(executed == false) then
-		executed = true;
-		for _, terr in pairs(game.ServerGame.LatestTurnStanding.Territories) do
-			currentarmies[terr.ID] = terr.NumArmies.NumArmies;
+
+	local publicGD = Mod.PublicGameData;
+	local cantRun = false;
+	local maxTerrs = 500;
+	local terrCount = 0;
+
+	for _ in pairs(game.Map.Territories) do
+		terrCount = terrCount + 1;
+
+		if terrCount > maxTerrs then
+			cantRun = true;
+			break;
 		end
-		for _, terr in pairs(game.ServerGame.LatestTurnStanding.Territories) do
-			if(terr.OwnerPlayerID == WL.PlayerID.Neutral)then
-				local Remainingarmies = currentarmies[terr.ID];
-				for _,conn in pairs(game.Map.Territories[terr.ID].ConnectedTo) do
-					if(game.ServerGame.LatestTurnStanding.Territories[conn.ID].OwnerPlayerID == WL.PlayerID.Neutral) then
-						local  Takenarmies = math.random(0,Remainingarmies);
-						if(Takenarmies > 0)then
-							Remainingarmies = Remainingarmies - Takenarmies;
-							currentarmies[terr.ID] = Remainingarmies;
-							currentarmies[conn.ID] = currentarmies[conn.ID] + Takenarmies;
-						end
+	end
+
+	publicGD.cantRun = cantRun;
+	Mod.PublicGameData = publicGD;
+end
+
+function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
+	if Mod.PublicGameData.cantRun then
+		return;
+	end
+
+	if order.proxyType == 'GameOrderCustom' and order.Payload == payload then
+		-- like this so that mod receives correct standing and no other mods change it
+		skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage);
+
+		if not executed then
+			main(game, addNewOrder);
+			executed = true;
+		end
+	end
+end
+
+function Server_AdvanceTurn_End(game, addNewOrder)
+	addNewOrder(WL.GameOrderCustom.Create(getFirstPlayer(game), '', payload));
+end
+
+function getFirstPlayer(game)
+	-- neutral isn't allowed for game order custom
+	for playerId in pairs(game.ServerGame.Game.Players) do
+		return playerId;
+	end
+end
+
+function main(game, addNewOrder)
+	if Mod.PublicGameData.cantRun then
+		return;
+	end
+
+	local armiesRemaining = {};
+	local armiesAdded = {};
+
+	for _, terr in pairs(game.ServerGame.LatestTurnStanding.Territories) do
+		if terr.OwnerPlayerID == WL.PlayerID.Neutral then
+			for _, conn in pairs(game.Map.Territories[terr.ID].ConnectedTo) do
+				local connTerr = game.ServerGame.LatestTurnStanding.Territories[conn.ID];
+
+				if connTerr.OwnerPlayerID == WL.PlayerID.Neutral then
+					local remaining = armiesRemaining[terr.ID] or terr.NumArmies.NumArmies;
+					local taken = math.random(0, remaining);
+
+					if taken > 0 then
+						armiesRemaining[terr.ID] = remaining - taken;
+						armiesRemaining[conn.ID] = (armiesRemaining[conn.ID] or connTerr.NumArmies.NumArmies) + taken;
+
+						armiesAdded[terr.ID] = (armiesAdded[terr.ID] or 0) - taken;
+						armiesAdded[conn.ID] = (armiesAdded[conn.ID] or 0) + taken;
 					end
 				end
 			end
 		end
-		for _, terr in pairs(game.ServerGame.LatestTurnStanding.Territories) do
-			terrMod[terrmodnum] = WL.TerritoryModification.Create(terr.ID);
-			terrMod[terrmodnum].SetArmiesTo = currentarmies[terr.ID];
-			terrmodnum = terrmodnum +1;
+	end
+
+	local terrMods = {};
+
+	for terrId, armiesToAdd in pairs(armiesAdded) do
+		if armiesToAdd ~= 0 then
+			local terrMod = WL.TerritoryModification.Create(terrId);
+			terrMod.AddArmies = armiesToAdd;
+			table.insert(terrMods, terrMod);
 		end
-		addNewOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral, 'Neutral_Move', {}, terrMod));
 	end
-end
-function tablelength(arr)
-	local num = 0;
-	for _,elem in pairs(arr)do
-		num = num + 1;
-	end
-	return num;
+
+	addNewOrder(WL.GameOrderEvent.Create(WL.PlayerID.Neutral, 'Neutral_Move', {}, terrMods));
 end
